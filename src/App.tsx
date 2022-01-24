88c41a939695e33c4f3a55d4aedbeb8e794e62a8
jabir <jabirhussainturi@gmail.com>
2022-01-24 20:54:55 +0500

    added some changes to check token between the apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+
 import { Dashboard } from "./pages/dashboard";
 import { Examples } from "pages/examples";
 import { Projects } from "pages/projects";
 
 function App() {
+  alert(
+    `AuthenticationToken: ${localStorage.getItem("jhi-authenticationToken")}`
+  );
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/examples/*" element={<Examples />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/vendors" element={<Projects />} />
+        <Route path="/vendorportal" element={<Dashboard />} />
+        <Route path="/vendorportal/examples/*" element={<Examples />} />
+        <Route path="/vendorportal/dashboard" element={<Dashboard />} />
+        <Route path="/vendorportal/projects" element={<Projects />} />
+        <Route path="/vendorportal/vendors" element={<Projects />} />
+        <Route path="*" element={<Navigate to="/vendorportal" />} />
       </Routes>
     </Router>
   );
