cf24422f9db70175c5dec085a2f43c936092e30f
jabir <jabirhussainturi@gmail.com>
2022-03-13 12:43:39 +0500

    change dashboard static vendor

--- lint-staged.js
+++ lint-staged.js
@@ -1,8 +1,9 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [
     'eslint --max-warnings=0',
-    'tsc-files --noEmit',
+    'tsc-files tsconfig.json --noEmit',
     'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
+    () => 'tsc-files --noEmit',
   ],
   '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
 }
