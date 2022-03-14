948825f5df68dc7b89e5b1db5c37121ec8b83a78
Maroof Ali <maroofalimehdi@gmail.com>
2022-03-14 18:51:10 +0500

    change fontFamily and fontSize in upload modal

--- src/features/projects/documents/upload-document.tsx
+++ src/features/projects/documents/upload-document.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -15,14 +15,14 @@ import {
   FormErrorMessage,
   VStack,
   ModalCloseButton,
-} from "@chakra-ui/react";
-import { MdOutlineCancel } from "react-icons/md";
-import { documentTypes, useUploadDocument } from "utils/vendor-projects";
+} from '@chakra-ui/react'
+import { MdOutlineCancel } from 'react-icons/md'
+import { documentTypes, useUploadDocument } from 'utils/vendor-projects'
 // import { t } from 'i18next';
-import { useTranslation } from "react-i18next";
-import { useUserProfile } from "utils/redux-common-selectors";
-import { Account } from "types/account.types";
-import { Document } from "types/vendor.types";
+import { useTranslation } from 'react-i18next'
+import { useUserProfile } from 'utils/redux-common-selectors'
+import { Account } from 'types/account.types'
+import { Document } from 'types/vendor.types'
 
 export const UploadDocumentModal: React.FC<any> = ({
   isOpen,
@@ -30,89 +30,89 @@ export const UploadDocumentModal: React.FC<any> = ({
   projectId,
   setLatestUploadedDoc,
 }) => {
-  const { t } = useTranslation();
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [document, setDocument] = useState<File | null>(null);
-  const [documentType, setDocumentType] = useState("");
-  const [isError, setError] = useState(false);
-  const { vendorId } = useUserProfile() as Account;
-  const { mutate: saveDocument } = useUploadDocument();
+  const { t } = useTranslation()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [document, setDocument] = useState<File | null>(null)
+  const [documentType, setDocumentType] = useState('')
+  const [isError, setError] = useState(false)
+  const { vendorId } = useUserProfile() as Account
+  const { mutate: saveDocument } = useUploadDocument()
 
   const onFileChange = useCallback(
-    (e) => {
-      const files = e.target.files;
+    e => {
+      const files = e.target.files
       if (files[0]) {
-        setDocument(files[0]);
+        setDocument(files[0])
       }
     },
-    [setDocument]
-  );
+    [setDocument],
+  )
 
   const resetUpload = useCallback(() => {
-    setDocument(null);
-    setDocumentType("");
-    setError(false);
+    setDocument(null)
+    setDocumentType('')
+    setError(false)
     if (inputRef.current) {
-      inputRef.current.value = "";
+      inputRef.current.value = ''
     }
-  }, [setDocument, setDocumentType, setError, inputRef]);
+  }, [setDocument, setDocumentType, setError, inputRef])
 
   const onDocumentTypeChange = useCallback(
-    (e) => {
-      if (e.target.value !== "") {
-        setError(false);
+    e => {
+      if (e.target.value !== '') {
+        setError(false)
       } else {
-        setError(true);
+        setError(true)
       }
-      setDocumentType(e.target.value);
+      setDocumentType(e.target.value)
     },
-    [setError, setDocumentType]
-  );
+    [setError, setDocumentType],
+  )
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   function readFile(event: any) {
-    const blob = event.target.result.split(",")[1];
+    const blob = event.target.result.split(',')[1]
     const doc: Document = {
       documentType,
       fileObject: blob,
-      fileObjectContentType: document?.type || "",
-      fileType: document?.name || "",
+      fileObjectContentType: document?.type || '',
+      fileType: document?.name || '',
       path: document?.name,
       projectId,
       vendorId,
-    };
+    }
     saveDocument(doc, {
       onSuccess() {
-        setLatestUploadedDoc(doc);
-        resetUpload();
-        onClose();
+        setLatestUploadedDoc(doc)
+        resetUpload()
+        onClose()
       },
-    });
+    })
   }
 
   const uploadDocument = useCallback(
-    (e) => {
-      if (documentType === "") {
-        setError(true);
-        return;
+    e => {
+      if (documentType === '') {
+        setError(true)
+        return
       }
       if (document) {
-        const reader = new FileReader();
-        reader.addEventListener("load", readFile);
-        reader.readAsDataURL(document);
+        const reader = new FileReader()
+        reader.addEventListener('load', readFile)
+        reader.readAsDataURL(document)
       } else {
-        return;
+        return
       }
     },
-    [documentType, document, setError, readFile]
-  );
+    [documentType, document, setError, readFile],
+  )
 
   return (
     <Modal
       isOpen={isOpen}
       onClose={() => {
-        resetUpload();
-        onClose();
+        resetUpload()
+        onClose()
       }}
       size="md"
     >
@@ -120,27 +120,41 @@ export const UploadDocumentModal: React.FC<any> = ({
       <ModalContent minW="71em" h="24em">
         <ModalHeader
           fontSize="18px"
+          fontWeight={700}
+          fontStyle="normal"
+          color="gray.700"
           borderBottom="1px solid #E2E8F0"
           borderTop="2px solid #4E87F8"
         >
-          {t("upload")}
+          {t('upload')}
         </ModalHeader>
-        <ModalCloseButton _focus={{ outline: "none" }} />
+        <ModalCloseButton _focus={{ outline: 'none' }} />
         <ModalBody>
           <FormControl mt="35px" isInvalid={isError}>
             <VStack align="start">
-              <FormLabel fontSize="16px" htmlFor="documentType">
-                {t("documentType")}{" "}
+              <FormLabel
+                fontSize="16px"
+                fontStyle="normal"
+                fontWeight={500}
+                color="gray.700"
+                htmlFor="documentType"
+              >
+                {t('documentType')}{' '}
               </FormLabel>
               <HStack spacing="20px">
                 <Select
+                  borderLeft="2px solid #4E87F8"
                   w="320px"
                   id="documentType"
                   placeholder="Select"
-                  size="lg"
+                  fontWeight={400}
+                  fontStyle="normal"
+                  fontSize="16px"
+                  color="gray.700"
+                  size="md"
                   onChange={onDocumentTypeChange}
                 >
-                  {documentTypes.map((type) => (
+                  {documentTypes.map(type => (
                     <option value={type.id} key={type.id}>
                       {type.value}
                     </option>
@@ -149,7 +163,7 @@ export const UploadDocumentModal: React.FC<any> = ({
                 <input
                   type="file"
                   ref={inputRef}
-                  style={{ display: "none" }}
+                  style={{ display: 'none' }}
                   onChange={onFileChange}
                 ></input>
                 {document ? (
@@ -178,24 +192,26 @@ export const UploadDocumentModal: React.FC<any> = ({
                       <MdOutlineCancel
                         cursor="pointer"
                         onClick={() => {
-                          setDocument(null);
-                          if (inputRef.current) inputRef.current.value = "";
+                          setDocument(null)
+                          if (inputRef.current) inputRef.current.value = ''
                         }}
                       />
                     </HStack>
                   </Box>
                 ) : (
                   <Button
-                    onClick={(e) => {
+                    onClick={e => {
                       if (inputRef.current) {
-                        inputRef.current.click();
+                        inputRef.current.click()
                       }
                     }}
-                    size="lg"
+                    fontSize="16px"
+                    fontWeight={600}
+                    fontStyle="normal"
                     variant="outline"
-                    color="barColor.100"
+                    color="#4E87F8"
                   >
-                    {t("chooseFile")}{" "}
+                    {t('chooseFile')}{' '}
                   </Button>
                 )}
               </HStack>
@@ -213,22 +229,29 @@ export const UploadDocumentModal: React.FC<any> = ({
           <Button
             variant="ghost"
             onClick={() => {
-              resetUpload();
-              onClose();
+              resetUpload()
+              onClose()
             }}
+            fontStyle="normal"
+            fontWeight={600}
+            fontSize="18px"
+            color="gray.700"
           >
-            {t("close")}
+            {t('close')}
           </Button>
           <Button
+            fontStyle="normal"
+            fontWeight={600}
+            fontSize="18px"
             onClick={uploadDocument}
-            colorScheme="button"
+            colorScheme="CustomPrimaryColor"
             type="submit"
             ml="3"
           >
-            {t("save")}
+            {t('save')}
           </Button>
         </ModalFooter>
       </ModalContent>
     </Modal>
-  );
-};
+  )
+}
