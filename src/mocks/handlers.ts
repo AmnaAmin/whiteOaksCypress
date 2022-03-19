d1bd78a97fb5730181c4bd3d6cffc91f94b8bfad
sania.siddiqui <sania.siddiqui@devtek.ai>
2022-03-19 12:33:27 +0500

    adding project detail handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,10 +2,12 @@ import { projectHandlers } from './api/projects/project-handlers'
 import { authenticationHandlers } from './api/authentication/authentication'
 import { vendorDashboardHandlers } from './api/vendor-dashboard/dashboard-handlers'
 import { vendorProfileHandlers } from './api/vendor-profile/vendor-profile-handlers'
+import { projectDetailHandlers } from './api/projects/project-details-handlers'
 
 export const handlers = [
   ...authenticationHandlers,
   ...projectHandlers,
   ...vendorDashboardHandlers,
   ...vendorProfileHandlers,
+  ...projectDetailHandlers,
 ]
