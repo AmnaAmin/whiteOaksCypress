591ca8341ae416b46c050081a6ca03f1054715ab
Maroof Ali <maroofalimehdi@gmail.com>
2022-04-13 00:51:11 +0500

    Ui of the left panel is created

--- src/pages/project-cordinator/routes.tsx
+++ src/pages/project-cordinator/routes.tsx
@@ -7,6 +7,10 @@ import { Projects } from 'pages/project-cordinator/projects'
 import { ProjectCoordinatorDashboard } from 'pages/project-cordinator/dashboard'
 import { ProjectDetails } from 'pages/project-cordinator/project-details'
 import { Payable } from './payable'
+import Receivable from './receivable'
+import Vendors from './vendors'
+import Clients from './clients'
+import Alerts from './alerts'
 
 export default function ProjectCordinatorRoutes() {
   return (
@@ -16,6 +20,10 @@ export default function ProjectCordinatorRoutes() {
       <Route path="/pcDashboard" element={<ProjectCoordinatorDashboard />} />
       <Route path="/project-details/:projectId" element={<ProjectDetails />} />
       <Route path="/payable" element={<Payable />} />
+      <Route path="/receivable" element={<Receivable />} />
+      <Route path="vendors" element={<Vendors />} />
+      <Route path="alerts" element={<Alerts />} />
+      <Route path="clients" element={<Clients />} />
       <Route path="/password" element={<VendorProfilePassword />} />
       <Route path="/support" element={<CreateATicket />} />
       <Route path="/settings" element={<Settings />} />
