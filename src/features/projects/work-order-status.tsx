b8e0857d17f5eef8c3481e467db0f9761dea912f
Maroof Ali <maroofalimehdi@gmail.com>
2022-04-13 15:49:55 +0500

    change color of cancelled tag

--- src/features/projects/work-order-status.tsx
+++ src/features/projects/work-order-status.tsx
@@ -19,8 +19,8 @@ const WorkOrderStatus = ({ value, id }: { value: string; id: string }) => {
       size="md"
       borderRadius="6px"
       variant="solid"
-      backgroundColor={WORK_ORDER_STATUS_COLOR[id]?.backgroundColor ?? '#6B46C1'}
-      color={WORK_ORDER_STATUS_COLOR[id]?.color ?? '#E9D8FD'}
+      backgroundColor={WORK_ORDER_STATUS_COLOR[id]?.backgroundColor ?? 'red.100'}
+      color={WORK_ORDER_STATUS_COLOR[id]?.color ?? 'red.400'}
     >
       <TagLabel fontSize="14px" fontWeight={400} fontStyle="normal" lineHeight="20px" p="3px">
         {value}
