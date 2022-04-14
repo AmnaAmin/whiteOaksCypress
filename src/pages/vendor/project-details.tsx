6c2d68da85bc25b2c98fee42e0839ae4577d6e74
jabir <jabirhussainturi@gmail.com>
2022-04-14 23:09:06 +0500

    Minor tweaks

--- src/pages/vendor/project-details.tsx
+++ src/pages/vendor/project-details.tsx
@@ -132,6 +132,7 @@ export const ProjectDetails: React.FC = props => {
                       selectedAlertRow(row.values)
                       onAlertModalOpen()
                     }}
+                    ref={tabsContainerRef}
                   />
                 </Box>
               </TabPanel>
