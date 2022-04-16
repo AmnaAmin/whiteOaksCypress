d32fa83a75a6140fe58d32f2369fc1092625f59e
sabih-maqsad <98734529+sabih-maqsad@users.noreply.github.com>
2022-04-16 23:38:35 +0500

    [DR] us-west-1

--- auto/deploy_dev.sh
+++ auto/deploy_dev.sh
@@ -7,3 +7,6 @@ unzip -o output.zip
 cd output
 echo "🚀 deploying to dev-s3"
 aws s3 sync . s3://woa-bucket-dev2-ui/vendorportal/ --delete
+
+echo "🚀 deploying to us-west-1-s3Bucket"
+aws s3 sync . s3://woa-fo-bucket-one-ui/vendorportal/ --delete
