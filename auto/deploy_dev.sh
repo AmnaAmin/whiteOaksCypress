853a0053d154ffc76d01a9be2375a3611b6d66ff
sabih-maqsad <98734529+sabih-maqsad@users.noreply.github.com>
2022-04-16 23:53:42 +0500

    [DR] us-east-1

--- auto/deploy_dev.sh
+++ auto/deploy_dev.sh
@@ -10,3 +10,6 @@ aws s3 sync . s3://woa-bucket-dev2-ui/vendorportal/ --delete
 
 echo "🚀 deploying to us-west-1-s3Bucket"
 aws s3 sync . s3://woa-fo-bucket-one-ui/vendorportal/ --delete
+
+echo "🚀 deploying to us-east-1-s3Bucket"
+aws s3 sync . s3://woa-fo-bucket-two-ui/vendorportal/ --delete
