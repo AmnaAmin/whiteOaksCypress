f69082cd3c6e32f09c7b3c1e1917a9e4dcb40173
Maroof Ali <maroofalimehdi@gmail.com>
2022-04-22 01:08:58 +0500

    Ui of Misc

--- src/features/project-coordinator/project-details/project-details-tab.tsx
+++ src/features/project-coordinator/project-details/project-details-tab.tsx
@@ -1,4 +1,4 @@
-import { HStack } from '@chakra-ui/react'
+import { Box, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { Card } from 'components/card/card'
@@ -12,63 +12,66 @@ const tabStyle = {
 
 const ProjectDetailsTab = () => {
   return (
-    <Card rounded="16px" padding="0">
-      <Tabs>
-        <TabList bg="#F7FAFC" rounded="6px 6px 0 0" pt="7">
-          <HStack spacing={5} ml="2">
-            <Tab
-              sx={tabStyle}
-              _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
-              _focus={{ outline: 'none' }}
-            >
-              Project Management
-            </Tab>
-            <Tab
-              sx={tabStyle}
-              _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
-              _focus={{ outline: 'none' }}
-            >
-              Invoicing & payment
-            </Tab>
-            <Tab
-              sx={tabStyle}
-              _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
-              _focus={{ outline: 'none' }}
-            >
-              Contacts
-            </Tab>
-            <Tab
-              sx={tabStyle}
-              _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
-              _focus={{ outline: 'none' }}
-            >
-              Location
-            </Tab>
-            <Tab
-              sx={tabStyle}
-              _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
-              _focus={{ outline: 'none' }}
-            >
-              Misc
-            </Tab>
-          </HStack>
-        </TabList>
+    <>
+      <Card rounded="16px" padding="0">
+        <Tabs>
+          <TabList bg="#F7FAFC" rounded="6px 6px 0 0" pt="7">
+            <HStack spacing={5} ml="2">
+              <Tab
+                sx={tabStyle}
+                _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
+                _focus={{ outline: 'none' }}
+              >
+                Project Management
+              </Tab>
+              <Tab
+                sx={tabStyle}
+                _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
+                _focus={{ outline: 'none' }}
+              >
+                Invoicing & payment
+              </Tab>
+              <Tab
+                sx={tabStyle}
+                _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
+                _focus={{ outline: 'none' }}
+              >
+                Contacts
+              </Tab>
+              <Tab
+                sx={tabStyle}
+                _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
+                _focus={{ outline: 'none' }}
+              >
+                Location
+              </Tab>
+              <Tab
+                sx={tabStyle}
+                _selected={{ borderBottom: '2px solid #4E87F8', fontWeight: 600, color: '#4E87F8' }}
+                _focus={{ outline: 'none' }}
+              >
+                Misc
+              </Tab>
+            </HStack>
+          </TabList>
 
-        <TabPanels>
-          <TabPanel></TabPanel>
-          <TabPanel>
-            <p>two!</p>
-          </TabPanel>
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-          <TabPanel mt="12" p="0" ml="6"></TabPanel>
-          <TabPanel mt="12" p="0" ml="6">
-            <Misc />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Card>
+          <TabPanels>
+            <TabPanel></TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+            <TabPanel mt="12" p="0" ml="6"></TabPanel>
+            <TabPanel mt="12" p="0" ml="6">
+              <Misc />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
+      <Box p="3"></Box>
+    </>
   )
 }
 
