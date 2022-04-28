ea18ecd0282e320e798c04afdc42124b43470b5a
Saqib Hussain <saqibtoori1@gmail.com>
2022-04-29 01:07:25 +0500

    notes ui changes

--- src/features/project-coordinator/notes-tab.tsx
+++ src/features/project-coordinator/notes-tab.tsx
@@ -8,96 +8,94 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
+  Text,
 } from '@chakra-ui/react'
 import { Button } from 'components/button/button'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
+import textArea from 'theme/components/textarea'
 
 export const NotesTab = () => {
+  const [addMessage, setAddMessage] = useState([] as any)
   const {
     handleSubmit,
     register,
     reset,
     formState: { errors },
   } = useForm()
+
   const Submit = data => {
     console.log(data)
+    setAddMessage(state => [...state, { ...data }])
     reset()
   }
 
   return (
     <form onSubmit={handleSubmit(Submit)}>
       <Box bg="white" rounded={16}>
-        <Box h={550} p={10} pb={5} overflow="auto">
-          <FormControl isInvalid={!!errors.input1?.message}>
-            <Flex mb={3}>
-              <Box mr={5} fontSize="12px" fontWeight={400} w="120px">
-                <WrapItem justifyContent="center" mb={1}>
-                  <Avatar size="sm" />
-                </WrapItem>
-                <Center color="gray.600">Admin@devtek.ai</Center>
-                <Center color="gray.500">02/22/2022</Center>
-              </Box>
-              <Box w="100%">
-                <Textarea
-                  py={7}
-                  minH="82px"
+        <Box h={550} p="40px 100px 0px 65px " overflow="auto">
+          <Flex mb={4}>
+            <Box mr={5} fontSize="12px" fontWeight={400}>
+              <WrapItem justifyContent="center" mb={1}>
+                <Avatar size="sm" bg="blackAlpha.200" />
+              </WrapItem>
+              <Center color="gray.600">Admin@devtek.ai</Center>
+              <Center color="gray.500">02/22/2022</Center>
+            </Box>
+            <Text
+              whiteSpace="pre-wrap"
+              w="100%"
+              p={7}
+              rounded={6}
+              bg="gray.50"
+              placeholder="Here is a sample placeholder"
+              border="1px solid #E2E8F0"
+            >
+              Hello
+            </Text>
+          </Flex>
+
+          {addMessage.map(message => {
+            return (
+              <Flex mb={4}>
+                <Box w="125px" />
+                <Text
+                  whiteSpace="pre-wrap"
+                  w="100%"
+                  p={7}
+                  rounded={6}
+                  bg="blue.50"
                   placeholder="Here is a sample placeholder"
-                  {...register('input1', { required: 'This field is required.' })}
+                  border="1px solid #E2E8F0"
+                >
+                  {message.textArea}
+                </Text>
+              </Flex>
+            )
+          })}
+
+          <FormControl isInvalid={!!errors.textArea?.message} mb={3}>
+            <Flex>
+              <Box w="125px" />
+              <Box w="100%">
+                <FormLabel fontSize="16px" color="gray.600" fontWeight={500}>
+                  Enter New Note Here
+                </FormLabel>
+                <Textarea
+                  id="value"
+                  flexWrap="wrap"
+                  minH="200px"
+                  placeholder="Here is a sample placeholder"
+                  {...register('textArea', { required: 'This field is required.' })}
                 />
-                <FormErrorMessage>{errors.input1?.message}</FormErrorMessage>
+                <FormErrorMessage>{errors.textArea?.message}</FormErrorMessage>
               </Box>
             </Flex>
-          </FormControl>
-
-          <FormControl isInvalid={!!errors.input2?.message}>
-            <Box mb={3} ml="130px">
-              <Textarea
-                py={7}
-                minH="82px"
-                placeholder="Here is a sample placeholder"
-                {...register('input2', { required: 'This field is required.' })}
-              />
-              <FormErrorMessage>{errors.input2?.message}</FormErrorMessage>
-            </Box>
-          </FormControl>
-
-          <FormControl isInvalid={!!errors.input3?.message}>
-            <Flex mb={3}>
-              <Box mr={5} fontSize="12px" fontWeight={400} w="120px">
-                <WrapItem justifyContent="center" mb={1}>
-                  <Avatar size="sm" />
-                </WrapItem>
-                <Center color="gray.600">Admin@devtek.ai</Center>
-                <Center color="gray.500">02/22/2022</Center>
-              </Box>
-              <Box w="100%">
-                <Textarea
-                  minH="127px"
-                  placeholder="Here is a sample placeholder"
-                  {...register('input3', { required: 'This field is required.' })}
-                />
-                <FormErrorMessage>{errors.input3?.message}</FormErrorMessage>
-              </Box>
-            </Flex>
-          </FormControl>
-
-          <FormControl isInvalid={!!errors.textArea?.message}>
-            <Box mb={3} ml="130px">
-              <FormLabel fontSize="16px" color="gray.600" fontWeight={500}>
-                Enter New Note Here
-              </FormLabel>
-              <Textarea
-                minH="118px"
-                placeholder="Here is a sample placeholder"
-                {...register('textArea', { required: 'This field is required.' })}
-              />
-              <FormErrorMessage>{errors.textArea?.message}</FormErrorMessage>
-            </Box>
           </FormControl>
         </Box>
 
         <Flex borderTop="1px solid #E2E8F0" h="92px" justifyContent="end" alignItems="center" pr={3}>
-          <Button type="submit" h="48px" w="130px" colorScheme="brand">
+          <Button type="submit" h="48px" w="130px" colorScheme="brand" isDisabled={textArea.Textarea ? false : true}>
             Submit
           </Button>
         </Flex>
