aa1bb1f8a34d3c2aa6eece368249ff2bd4093664
Saqib Hussain <saqibtoori1@gmail.com>
2022-04-30 19:05:39 +0500

    in pc project client ui and integration completed

---
+++ src/features/project-coordinator/clients-flie/side-bar-clients.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import { Box, Td, Tr, Text, Flex } from '@chakra-ui/react'
+import { useColumnWidthResize } from 'utils/hooks/useColumnsWidthResize'
+import ReactTable, { RowProps } from 'components/table/react-table'
+import { usePcClients } from 'utils/pc-projects'
+// import { PieChart } from 'recharts'
+
+// enum PROJECT_CATEGORY {
+//   WARNING = 1,
+//   INFO = 2,
+//   ERROR = 3,
+// }
+
+const alertsRow: React.FC<RowProps> = ({ row, style, onRowClick }) => {
+  return (
+    <Tr
+      bg="white"
+      _hover={{
+        background: '#eee',
+      }}
+      onClick={e => {
+        if (onRowClick) {
+          onRowClick(e, row)
+        }
+      }}
+      {...row.getRowProps({
+        style,
+      })}
+    >
+      {row.cells.map(cell => {
+        return (
+          <Td {...cell.getCellProps()} key={`row_${cell.value}`} p="0">
+            <Flex alignItems="center" h="60px">
+              <Text noOfLines={2} title={cell.value} padding="0 15px" color="blackAlpha.600">
+                {cell.render('Cell')}
+              </Text>
+            </Flex>
+          </Td>
+        )
+      })}
+    </Tr>
+  )
+}
+
+export const SideBarClients = React.forwardRef((props: any, ref) => {
+  const { data: PcData } = usePcClients()
+
+  const { columns, resizeElementRef } = useColumnWidthResize(
+    [
+      {
+        Header: 'Name',
+        accessor: 'companyName',
+      },
+      {
+        Header: 'Contact',
+        accessor: 'contacts[0].contact',
+      },
+      {
+        Header: 'Address',
+        accessor: 'streetAddress',
+      },
+      {
+        Header: 'Phone',
+        accessor: 'contacts[0].phoneNumber',
+      },
+      {
+        Header: 'Email',
+        accessor: 'contacts[0].emailAddress',
+        // Cell: ({ value }) => PROJECT_CATEGORY[value],
+      },
+      {
+        Header: 'Contact',
+        accessor: 'accountPayableContactInfos[0].contact',
+      },
+      {
+        Header: 'Email',
+        accessor: 'accountPayableContactInfos[0].emailAddress',
+      },
+
+      {
+        Header: 'Phone',
+        accessor: 'accountPayableContactInfos[0].phoneNumber',
+        // Cell: ({ value }) => dateFormat(value),
+      },
+    ],
+    ref,
+  )
+
+  return (
+    <Box ref={resizeElementRef}>
+      <ReactTable
+        onRowClick={props.onRowClick}
+        columns={columns}
+        data={PcData || []}
+        TableRow={alertsRow}
+        tableHeight="calc(100vh - 300px)"
+        name="alerts-table"
+      />
+    </Box>
+  )
+})
