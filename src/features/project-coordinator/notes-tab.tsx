d92b510e3c1533e9c1b7184729b39c3fc43ebe31
Saqib Hussain <saqibtoori1@gmail.com>
2022-05-11 19:12:38 +0500

    resolvedfeedback

--- src/features/project-coordinator/notes-tab.tsx
+++ src/features/project-coordinator/notes-tab.tsx
@@ -2,7 +2,38 @@ import { Avatar, Box, Flex, Textarea, WrapItem, Center, FormLabel, Text } from '
 import { Button } from 'components/button/button'
 import { useState } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
-import notesAddminMassage from './notes-data.json'
+import notesAddminData from './notes-admin-data.json'
+
+const MessagesTypes: React.FC<{ user?: string; other?: string }> = ({ user, other }) => {
+  return (
+    <Flex mb={4}>
+      {other ? (
+        <Box mr={5} fontSize="12px" fontWeight={400}>
+          <WrapItem justifyContent="center" mb={1}>
+            <Avatar size="sm" bg="blackAlpha.200" />
+          </WrapItem>
+          <Center color="gray.600">Admin@devtek.ai</Center>
+          <Center color="gray.500">02/22/2022</Center>
+        </Box>
+      ) : (
+        <Box w="115px" />
+      )}
+      <Text
+        whiteSpace="pre-wrap"
+        w="100%"
+        p={7}
+        rounded={6}
+        bg={user ? 'blue.50' : 'gray.50'}
+        border="1px solid #E2E8F0"
+        flex="1"
+        wordBreak="break-all"
+      >
+        {other}
+        {user}
+      </Text>
+    </Flex>
+  )
+}
 
 export const NotesTab = () => {
   const [messages, setMessage] = useState([] as any)
@@ -18,66 +49,23 @@ export const NotesTab = () => {
     <form onSubmit={handleSubmit(Submit)}>
       <Box bg="white" rounded={16}>
         <Box p="40px 100px 0px 65px " overflow="auto">
-          <Box h={400} zIndex={1} overflow="auto">
-            {notesAddminMassage.map(massages => {
-              return (
-                <Flex mb={4}>
-                  <Box mr={5} fontSize="12px" fontWeight={400}>
-                    <WrapItem justifyContent="center" mb={1}>
-                      <Avatar size="sm" bg="blackAlpha.200" />
-                    </WrapItem>
-                    <Center color="gray.600">Admin@devtek.ai</Center>
-                    <Center color="gray.500">02/22/2022</Center>
-                  </Box>
-                  <Text
-                    whiteSpace="pre-wrap"
-                    w="100%"
-                    p={7}
-                    rounded={6}
-                    bg="gray.50"
-                    placeholder="Here is a sample placeholder"
-                    border="1px solid #E2E8F0"
-                    flex="1"
-                    wordBreak="break-all"
-                  >
-                    {massages.massage}
-                  </Text>
-                </Flex>
-              )
+          <Box h={390} overflow="auto">
+            {notesAddminData.map(data => {
+              return <MessagesTypes other={data.message} />
             })}
 
             {messages.map(message => {
-              return (
-                <Flex mb={4}>
-                  <Box w="115px" />
-                  <Text
-                    p={7}
-                    rounded={6}
-                    bg="blue.50"
-                    placeholder="Here is a sample placeholder"
-                    border="1px solid #E2E8F0"
-                    flex="1"
-                    wordBreak="break-all"
-                  >
-                    {message}
-                  </Text>
-                </Flex>
-              )
+              return <MessagesTypes user={message} />
             })}
           </Box>
 
-          <Flex zIndex={2}>
+          <Flex>
             <Box w="125px" />
             <Box w="100%">
               <FormLabel fontSize="16px" color="gray.600" fontWeight={500}>
                 Enter New Note Here
               </FormLabel>
-              <Textarea
-                flexWrap="wrap"
-                minH="200px"
-                placeholder="Here is a sample placeholder"
-                {...register('message')}
-              />
+              <Textarea flexWrap="wrap" minH="200px" {...register('message')} />
             </Box>
           </Flex>
         </Box>
