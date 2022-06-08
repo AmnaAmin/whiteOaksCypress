e48a9b34da4ad00409762535d6a9918905bcae9f
Yusra Adil <yusraadil91@gmail.com>
2022-06-08 17:37:24 +0500

    Alerts - Triggered Alert UI

--- src/pages/project-cordinator/alerts.tsx
+++ src/pages/project-cordinator/alerts.tsx
@@ -1,8 +1,57 @@
-import { Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Stack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { TriggeredAlertsTable } from 'features/project-coordinator/alerts/triggered-alerts'
+import { useState } from 'react'
 
 const Alerts = () => {
-  return <Heading>Alerts Page...</Heading>
+  const [tabIndex, setTabIndex] = useState(0)
+
+  return (
+    <>
+      <Stack w={{ base: '971px', xl: '100%' }}>
+        <Tabs variant="enclosed" colorScheme="brand" onChange={index => setTabIndex(index)} mt="7">
+          <TabList>
+            <Tab minW={180}>Triggered Alerts</Tab>
+            <Tab minW={180}>Managed Alerts</Tab>
+            <Box w="100%" display="flex" justifyContent="end" position="relative">
+              {tabIndex === 0 && (
+                <Button
+                  variant="solid"
+                  colorScheme="brand"
+                  // onClick={resolveTriggered}
+                  mb={2}
+                >
+                  {'Resolve All'}
+                </Button>
+              )}
+              {tabIndex === 1 && (
+                <Button
+                  // onClick={resolveManaged}
+                  color="white"
+                  size="md"
+                  bg="#4e87f8"
+                  _hover={{ bg: '#2A61CE' }}
+                  mb={2}
+                >
+                  {'Resolve All'}
+                </Button>
+              )}
+            </Box>
+          </TabList>
+
+          <TabPanels h="100%">
+            <TabPanel p="0px" h="100%" mt="7px">
+              <Box h="100%">
+                <TriggeredAlertsTable />
+              </Box>
+            </TabPanel>
+            <TabPanel p="0px" h="100%" mt="7px">
+              <Box h="100%">{/* <ManagedAlertsTable /> */}</Box>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Stack>
+    </>
+  )
 }
 
 export default Alerts
