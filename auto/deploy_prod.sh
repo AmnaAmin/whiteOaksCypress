fec61e54c5834ce588067db8272f833f84c50817
Mohammad Sabih Ahmed <96499255+Sabih-DevTek@users.noreply.github.com>
2022-06-16 03:55:26 +0500

    Update Prod.

--- auto/deploy_prod.sh
+++ auto/deploy_prod.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
 set -eux
-# account # 324351813761
+
 PARENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 echo "🚀 Deploying project to Prod..."
 
-# buildkite-agent artifact download output.zip . --build $BUILDKITE_JOB_ID
-# unzip -o output.zip
 cd output
+
 echo "🚀 deploying to s3"
 aws s3 sync . s3://whiteoaks-manual-next-gen-ui --delete
