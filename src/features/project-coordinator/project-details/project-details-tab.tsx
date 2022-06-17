4f87eb56506d4e5919566fbbda265e15cb638d69
Moajiz1 <moajiznaqvi@gmail.com>
2022-06-17 15:22:54 +0500

    button bug resolved

--- src/features/project-coordinator/project-details/project-details-tab.tsx
+++ src/features/project-coordinator/project-details/project-details-tab.tsx
@@ -243,7 +243,7 @@ const ProjectDetailsTab = (props: tabProps) => {
               <Box mt="3">
                 <Divider border="1px solid" />
               </Box>
-              <Box h="70px" w="100%" pb="3">
+              <Box h="69px" w="100%" pb="3">
                 <Button
                   mt="8px"
                   mr="32px"
