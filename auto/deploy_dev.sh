93821563cfc82bbd9763e8a9eee28727d04e5bde
Mohammad Sabih Ahmed <96499255+Sabih-DevTek@users.noreply.github.com>
2022-06-19 11:19:41 +0500

    Update Buildkite Configurations for Dev2

--- auto/deploy_dev.sh
+++ auto/deploy_dev.sh
@@ -6,9 +6,28 @@ PARENT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 echo "🚀 Deploying project to Dev..."
 
+if [ "dev" = $ENV ]; then
 buildkite-agent artifact download output.zip .
 unzip -o output.zip
 cd output
 
 echo "🚀 deploying to s3"
 aws s3 sync . s3://woa-dev-v2-ui/vendorportal/ --delete
+else
+  echo "Dev Env. 404"
+  exit 1
+fi
+
+
+
+if [ "dev2" = $ENV ]; then
+buildkite-agent artifact download output.zip .
+unzip -o output.zip
+cd output
+
+echo "🚀 deploying to s3"
+aws s3 sync . s3://woa-dev-ui/vendorportal/ --delete
+else
+  echo "Dev2 Env. 404"
+  exit 1
+fi
