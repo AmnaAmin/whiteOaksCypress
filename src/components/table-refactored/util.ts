d40a6caccc131fee39eb22d6b5ccf1d1cbbae61b
tahir iqbal <tahiir500@gmail.com>
2022-07-26 10:41:31 +0500

    remove moment library

--- src/components/table-refactored/util.ts
+++ src/components/table-refactored/util.ts
@@ -1,6 +1,6 @@
 import * as XLSX from 'xlsx'
-import moment from 'moment'
 import { APP_LOCAL_DATE_FORMAT_Z } from 'components/layout/constants'
+import { format } from 'date-fns'
 
 export const getFileBlob = ({ columns, data, fileType, fileName }) => {
   const header = columns.map(c => c.exportValue)
@@ -114,7 +114,9 @@ export const convertImageToDataURL = (src, callback) => {
 }
 
 export const convertDateTimeToServerISO = date => {
-  return date ? moment(date, APP_LOCAL_DATE_FORMAT_Z).toISOString() : null
+  if (!date) return null
+  const formatDate = format(new Date(date), APP_LOCAL_DATE_FORMAT_Z)
+  return new Date(formatDate).toISOString()
 }
 
 export const convertImageUrltoDataURL = url =>
