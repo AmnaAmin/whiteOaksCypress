0a9af533f3130389a77028a214123377315876d8
haider-sarfraz <m.haidersarfraz@gmail.com>
2022-07-27 20:37:45 +0500

    Fix Table height issue in TableWrapper with Jabir

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -36,15 +36,13 @@ export const TableWrapper: React.FC<TableProps & TableExtraProps> = props => {
   }, [tableInstance, setTableInstance])
 
   return (
-    <Stack h="100%">
-      <Table tableInstance={tableInstance} tableHeight={tableHeight}>
-        <TableHead {...tableInstance} />
-        {isLoading ? (
-          <TableLoadingState {...tableInstance} />
-        ) : (
-          <TableBody {...tableInstance} onRowClick={onRowClick} TableRow={TableRow} />
-        )}
-      </Table>
-    </Stack>
+    <Table tableInstance={tableInstance} tableHeight={tableHeight}>
+      <TableHead {...tableInstance} />
+      {isLoading ? (
+        <TableLoadingState {...tableInstance} />
+      ) : (
+        <TableBody {...tableInstance} onRowClick={onRowClick} TableRow={TableRow} />
+      )}
+    </Table>
   )
 }
