ac569df527bb1d51cfebc7d60067fafe2617415c
yusraadil <yusraadil9a@gmail.com>
2022-08-09 17:44:59 +0500

    Change UI of Project Summary Cards

--- src/features/project-coordinator/transaction-info-card-details.tsx
+++ src/features/project-coordinator/transaction-info-card-details.tsx
@@ -7,11 +7,13 @@ import { useAuth } from 'utils/auth-context'
 
 const InfoStructureCard: React.FC<{ isLoading: boolean } & CenterProps> = ({ children, isLoading, title, ...rest }) => {
   return (
-    <Center flexDir="column" px={4}>
-      <Box fontSize="14px" color="gray.500">
-        <FormLabel variant="strong-label" size="md">
-          {title}
-        </FormLabel>
+    <Center flexDir="column" borderRight="1px solid #E5E5E5" px={4} flex={rest.flex || 1} {...rest}>
+      <Box fontSize="14px" color="gray.500" width={'100px'}>
+        <Tooltip label={title} color="black" placement="top">
+          <FormLabel variant="strong-label" size="md" noOfLines={1}>
+            {title}
+          </FormLabel>
+        </Tooltip>
         {isLoading ? <BlankSlate size="sm" /> : children}
       </Box>
     </Center>
@@ -35,98 +37,53 @@ export const TransactionInfoCardDetails: React.FC<{
         borderRadius="4px"
         box-shadow="0px 20px 70px rgba(86, 89, 146, 0.1)"
       >
-        <VStack borderRight="1px solid #E5E5E5" px={2} flex={2}>
-          <HStack>
-            <Box>
-              <InfoStructureCard title={'Project Name'} isLoading={isLoading} />
-            </Box>
-            <Box width="125px">
-              <FormLabel variant="light-label" size="md" noOfLines={1}>
-                {projectData?.name}
-              </FormLabel>
-            </Box>
-          </HStack>
-          <HStack>
-            <Box marginRight={2}>
-              <InfoStructureCard title={'Project Type'} isLoading={isLoading} />
-            </Box>
-            <Box width="125px">
-              <FormLabel variant="light-label" size="md">
-                {projectData?.projectTypeLabel}
-              </FormLabel>
-            </Box>
-          </HStack>
-        </VStack>
-        <VStack borderRight="1px solid #E5E5E5" flex={2}>
-          <HStack>
-            <Box>
-              <InfoStructureCard title={'Client Start Date'} isLoading={isLoading} />
-            </Box>
-            <Box>
-              <FormLabel variant="light-label" size="md" width="90px">
-                {dateFormat(projectData?.clientStartDate as string)}
-              </FormLabel>
-            </Box>
-          </HStack>
-          <HStack>
-            <Box>
-              <InfoStructureCard title={'Client End Date'} isLoading={isLoading} />
-            </Box>
-            <Box>
-              <FormLabel variant="light-label" size="md" width="90px" ml={1}>
-                {dateFormat(projectData?.clientDueDate as string)}
-              </FormLabel>
-            </Box>
-          </HStack>
-        </VStack>
-        <VStack borderRight="1px solid #E5E5E5" flex={2}>
-          <HStack>
-            <Box>
-              <InfoStructureCard title={'WOA Start Date'} isLoading={isLoading} />
-            </Box>
-            <Box>
-              <FormLabel variant="light-label" size="md" width="100px">
-                {dateFormat(projectData?.woaStartDate as string)}
-              </FormLabel>
-            </Box>
-          </HStack>
-          <HStack>
-            <Box>
-              <InfoStructureCard title={'WOA End Date'} isLoading={isLoading} />
-            </Box>
-            <Box>
-              <FormLabel variant="light-label" size="md" width="100px" ml={1}>
-                {dateFormat(projectData?.woaCompletionDate as string)}
-              </FormLabel>
-            </Box>
-          </HStack>
-        </VStack>
-        <VStack flex={2}>
-          <HStack>
-            <Tooltip label="Project Coordinator Name" color="black" placement="top">
-              <Box>
-                <InfoStructureCard title={'PC Name'} isLoading={isLoading} />
-              </Box>
-            </Tooltip>
-            <Box paddingLeft={3}>
-              <FormLabel variant="light-label" size="md" width="100px">
-                {projectData?.projectCoordinator}
-              </FormLabel>
-            </Box>
-          </HStack>
-          <HStack>
-            <Tooltip label="Project Coordinator Contact" color="black">
-              <Box>
-                <InfoStructureCard title={'PC Contact'} isLoading={isLoading} />
-              </Box>
-            </Tooltip>
-            <Box>
-              <FormLabel variant="light-label" size="md" width="100px">
-                {account?.telephoneNumber}
-              </FormLabel>
-            </Box>
-          </HStack>
-        </VStack>
+        <InfoStructureCard title={'Project Name'} isLoading={isLoading} w={'250px'}>
+          <FormLabel variant="light-label" size="md" noOfLines={1}>
+            {projectData?.name}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'Project Type'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md">
+            {projectData?.projectTypeLabel}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'Client Start Date'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="90px">
+            {dateFormat(projectData?.clientStartDate as string)}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'Client End Date'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="90px" ml={1}>
+            {dateFormat(projectData?.clientDueDate as string)}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'WOA Start Date'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="100px">
+            {dateFormat(projectData?.woaStartDate as string)}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'WOA End Date'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="100px" ml={1}>
+            {projectData?.woaCompletionDate ? dateFormat(projectData?.woaCompletionDate as string) : '....'}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'Project Coordinator Name'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="100px">
+            {projectData?.projectCoordinator}
+          </FormLabel>
+        </InfoStructureCard>
+
+        <InfoStructureCard title={'Project Coordinator Contact'} isLoading={isLoading}>
+          <FormLabel variant="light-label" size="md" width="100px">
+            {account?.telephoneNumber}
+          </FormLabel>
+        </InfoStructureCard>
       </Flex>
     </>
   )
