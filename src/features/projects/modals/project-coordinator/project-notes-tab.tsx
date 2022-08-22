4ac30d23572f11c55bc7d24017b631068948f890
sania.siddiqui <sania.siddiqui@devtek.ai>
2022-08-22 15:30:57 +0500

    woa-fix-notes

--- src/features/projects/modals/project-coordinator/project-notes-tab.tsx
+++ src/features/projects/modals/project-coordinator/project-notes-tab.tsx
@@ -16,7 +16,7 @@ export const ProjectNotes: React.FC<any> = props => {
   })
 
   useEffect(() => {
-    if (notes && notes.length > 0) {
+    if (notes?.length > 0 && setNotesCount) {
       setNotesCount(notes.filter((note: any) => note.createdBy !== account.login)?.length)
     }
   }, [notes])
