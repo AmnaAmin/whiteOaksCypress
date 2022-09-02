8338e1bbcf299a43a223f88d03805ec77c12c8d7
tahir iqbal <tahiir500@gmail.com>
2022-09-02 23:12:08 +0500

    lint fix

--- src/utils/date-time-utils.ts
+++ src/utils/date-time-utils.ts
@@ -14,19 +14,6 @@ export const datePickerFormat = (date: string | Date) => {
   return date ? format(new Date(date), 'yyyy-MM-dd') : null
 }
 
-/**
- * Returns a date with localized midnight timestamp
- * by adjusting the UTC minutes with timezoneOffset
- * @param date string
- * @returns 
- */
- export const getLocalTimeZoneDate = (date: string) => {
-  if(!date) return null;
-  const dateObj = new Date(date);
-  dateObj.setUTCMinutes(dateObj.getTimezoneOffset());
-  return datePickerFormat(dateObj);
-}
-
 export const dateISOFormat = (date: string | Date | null) => {
   if (date === null) return null
 
@@ -45,13 +32,13 @@ export const getFormattedDate = (date: Date) => {
  * Returns a date with localized midnight timestamp
  * by adjusting the UTC minutes with timezoneOffset
  * @param date string
- * @returns 
+ * @returns
  */
 export const getLocalTimeZoneDate = (date: string) => {
-  if(!date) return null;
-  const dateObj = new Date(date);
-  dateObj.setUTCMinutes(dateObj.getTimezoneOffset());
-  return datePickerFormat(dateObj);
+  if (!date) return null
+  const dateObj = new Date(date)
+  dateObj.setUTCMinutes(dateObj.getTimezoneOffset())
+  return datePickerFormat(dateObj)
 }
 
 export type MonthOption = {
