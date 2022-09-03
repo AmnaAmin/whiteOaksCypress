12ef7dd2ecab2026b3130dfe1004c97161c393e2
HafizSarwar <100361550+HafizSarwar@users.noreply.github.com>
2022-09-04 02:20:09 +0500

    Update reports.tsx

--- src/pages/reports.tsx
+++ src/pages/reports.tsx
@@ -1,4 +1,4 @@
-import { Box,  Flex, Text } from '@chakra-ui/react'
+import { Box,  Flex, Text, Button } from '@chakra-ui/react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate} from "react-router-dom";
 
