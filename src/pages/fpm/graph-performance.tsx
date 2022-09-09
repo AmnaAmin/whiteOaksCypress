02185b547c7874d30aeabd370ed10859891b752b
Moajiz1 <moajiznaqvi@gmail.com>
2022-09-09 08:48:17 +0500

    minor integration left

---
+++ src/pages/fpm/graph-performance.tsx
@@ -0,0 +1,165 @@
+import { Box } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import { GenericObjectType } from 'types/common.types'
+
+export enum WORK_ORDER_STATUS {
+  Paid = 68,
+  Active = 34,
+  Completed = 36,
+  Cancelled = 35,
+  Inactive = 37,
+  Invoiced = 110,
+  Decline = 111,
+  PastDue = 114,
+}
+
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+export const monthsShort: GenericObjectType = {
+  January: 'Jan',
+  February: 'Feb',
+  March: 'Mar',
+  April: 'Apr',
+  May: 'May',
+  June: 'Jun',
+  July: 'Jul',
+  August: 'Aug',
+  September: 'Sep',
+  October: 'Oct',
+  November: 'Nov',
+  December: 'Dec',
+}
+
+const PerformanceGraph: React.FC<{ chartData?: any }> = ({ chartData }) => {
+  const vendors = [chartData?.chart]
+
+  const vendorData = months.map(key => {
+    const monthExistsInChart = chartData !== undefined && Object.keys(vendors[0])?.find(months => months === key)
+    let nameMonthData
+    if (monthExistsInChart) {
+      nameMonthData = chartData?.chart[key]
+    }
+    return {
+      name: monthsShort[key],
+      Bonus: nameMonthData?.bonus,
+      Profit: nameMonthData?.profit,
+      Revenue: nameMonthData?.revenue,
+    }
+  })
+
+  return <OverviewGraph vendorData={vendorData} width="98%" height={360} />
+}
+
+export const OverviewGraph = ({ vendorData, width, height }) => {
+  const labels = [
+    { key: 'Bonus', color: '#FB8832' },
+    { key: 'Profit', color: '#949AC2' },
+    { key: 'Revenue', color: '#68B8EF' },
+  ]
+
+  const [barProps, setBarProps] = useState(
+    labels.reduce(
+      (a, { key }) => {
+        a[key] = false
+        return a
+      },
+      { hover: null },
+    ),
+  )
+
+  const selectBar = e => {
+    setBarProps({
+      ...barProps,
+      [e.dataKey]: !barProps[e.dataKey],
+      hover: null,
+    })
+  }
+  return (
+    <div>
+      <ResponsiveContainer width={width} height={height}>
+        <BarChart
+          data-testid="overview-chart"
+          data={vendorData}
+          barSize={50}
+          margin={{
+            top: 14,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid stroke="#EFF3F9" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tickLine={false}
+            tick={{
+              fill: '#4A5568',
+              fontSize: '12px',
+              fontWeight: 400,
+              fontStyle: 'normal',
+            }}
+            tickMargin={20}
+          />
+
+          <YAxis
+            tickLine={{ stroke: '#4F4F4F' }}
+            type="number"
+            tickSize={8}
+            tickCount={7}
+            domain={[0]}
+            axisLine={false}
+            tick={{
+              fontSize: '12px',
+              fontStyle: 'normal',
+              fontWeight: 400,
+              fill: '#4A5568',
+            }}
+          />
+
+          <Tooltip contentStyle={{ borderRadius: '6px' }} data-testid="tooltip-overview" cursor={{ fill: '#EBF8FF' }} />
+
+          <Bar dataKey="Bonus" fill="#FB8832" radius={[10, 10, 0, 0]} hide={barProps['Bonus'] === true} />
+          <Bar dataKey="Profit" fill="#949AC2" radius={[10, 10, 0, 0]} hide={barProps['Profit'] === true} />
+          <Bar dataKey="Revenue" fill="#68B8EF" radius={[10, 10, 0, 0]} hide={barProps['Revenue'] === true} />
+          <Legend
+            onClick={selectBar}
+            wrapperStyle={{
+              lineHeight: '31px',
+              position: 'relative',
+              bottom: 'calc(100% + 35px)',
+              left: '36px',
+            }}
+            iconType="circle"
+            iconSize={10}
+            align="right"
+            formatter={value => {
+              return (
+                <Box display="inline-flex" marginInlineEnd="30px" data-testid={'legend-' + value}>
+                  <Box as="span" color="gray.600" fontSize="12px" fontStyle="normal" fontWeight={400}>
+                    {value}
+                  </Box>
+                </Box>
+              )
+            }}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default PerformanceGraph
