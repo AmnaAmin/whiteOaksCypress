ff1f979adda523fef0169a1e7841353dfb488a4a
Saqib Hussain <saqibtoori1@gmail.com>
2022-09-28 17:42:47 +0500

    woa-4540

--- src/features/projects/new-project/manage-project.tsx
+++ src/features/projects/new-project/manage-project.tsx
@@ -110,7 +110,7 @@ export const ManageProject: React.FC<{
           <GridItem>
             <FormControl>
               <FormLabel size="md">{t(`${NEW_PROJECT}.clientSuperName`)}</FormLabel>
-              <Input id="clientSuperName" {...register('superFirstName')} />
+              <Input id="clientSuperName" {...register('superLastName')} />
             </FormControl>
           </GridItem>
         </Grid>
