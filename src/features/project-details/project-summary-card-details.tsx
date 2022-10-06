ec042f44c55c980cab9ea1f669eacc8024552161
Ali Moajiz <ali.moajiz@devtek.ai>
2022-10-06 20:20:41 +0500

    checking husky done

--- src/features/project-details/project-summary-card-details.tsx
+++ src/features/project-details/project-summary-card-details.tsx
@@ -37,10 +37,9 @@ export const ProjectSummaryCardDetails: React.FC<{
         borderRadius="4px"
         box-shadow="0px 20px 70px rgba(86, 89, 146, 0.1)"
       >
-        {/* woa-4612 */}
         <InfoStructureCard title={'Lock Box Code'} isLoading={isLoading}>
           <FormLabel variant="light-label" size="md" noOfLines={1}>
-            {projectData?.name}
+            {projectData?.lockBoxCode}
           </FormLabel>
         </InfoStructureCard>
 
