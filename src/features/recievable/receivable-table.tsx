b07e93b8642ca46e333582a301fe1aa8c4f817cf
jabir <jabirhussainturi@gmail.com>
2022-10-15 23:44:09 +0500

    Unsafe changes for receivable table refactoring

--- src/features/recievable/receivable-table.tsx
+++ src/features/recievable/receivable-table.tsx
@@ -1,126 +1,76 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { Box, Td, Tr, Text, Flex, useDisclosure } from '@chakra-ui/react'
-import { TableWrapper } from 'components/table/table'
-import { RowProps } from 'components/table/react-table'
-import AccountReceivableModal from 'features/recievable/account-receivable-modal'
-import { usePCRecievable } from 'api/account-receivable'
-import UpdateTransactionModal from 'features/project-details/transactions/update-transaction-modal'
-import { Column } from 'react-table'
-
-const ReceivableRow: React.FC<RowProps> = ({ row, style, onRowClick }) => {
-  return (
-    <Tr
-      bg="white"
-      _hover={{
-        background: '#eee',
-      }}
-      onClick={e => {
-        if (onRowClick) {
-          onRowClick(e, row)
-        }
-      }}
-      {...row.getRowProps({
-        style,
-      })}
-    >
-      {row.cells.map(cell => {
-        return (
-          <Td {...cell.getCellProps()} key={`row_${cell.column.id}`} p="0">
-            <Flex alignItems="center" h="60px">
-              <Text
-                noOfLines={1}
-                title={cell.value}
-                padding="0 15px"
-                color="gray.600"
-                mb="20px"
-                mt="10px"
-                fontSize="14px"
-                fontStyle="normal"
-                fontWeight="400"
-              >
-                {cell.render('Cell')}
-              </Text>
-            </Flex>
-          </Td>
-        )
-      })}
-    </Tr>
-  )
-}
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+// import AccountReceivableModal from 'features/recievable/account-receivable-modal'
+// import { usePCRecievable } from 'api/account-receivable'
+// import UpdateTransactionModal from 'features/project-details/transactions/update-transaction-modal'
+import { ColumnDef, ColumnFiltersState, PaginationState, Updater } from '@tanstack/react-table'
+// import { TableContextProvider } from 'components/table-refactored/table-context'
 
 type ReceivableProps = {
-  selectedCard: string
-  selectedDay: string
-  resizeElementRef?: any
-  setTableInstance: (tableInstance: any) => void
-  receivableColumns: Column[]
-  weeklyCount?: { name: string; title: string; count: number; dates: Date }[]
-  weekDayFilters: any[]
+  receivableColumns: ColumnDef<any>[]
+  setPagination: (updater: Updater<PaginationState>) => void
+  setColumnFilters: (updater: Updater<ColumnFiltersState>) => void
+  pagination: PaginationState
+  queryStringWithPagination: string
+  queryStringWithoutPagination: string
 }
 
-export const ReceivableTable: React.FC<ReceivableProps> = ({
-  setTableInstance,
-  selectedCard,
-  resizeElementRef,
-  receivableColumns,
-  selectedDay,
-  weekDayFilters,
-}) => {
-  const [selectedTransactionId, setSelectedTransactionId] = useState<number>()
-  const [selectedProjectId, setSelectedProjectId] = useState<string>()
-  const {
-    isOpen: isAccountReceivableModal,
-    onOpen: onAccountReceivableModalOpen,
-    onClose: onAccountReceivableModalClose,
-  } = useDisclosure()
-  const { isOpen: isOpenTransactionModal, onOpen: onEditModalOpen, onClose: onTransactionModalClose } = useDisclosure()
+export const ReceivableTable: React.FC<ReceivableProps> = () => {
+  // const [selectedTransactionId, setSelectedTransactionId] = useState<number>()
+  // const [selectedProjectId, setSelectedProjectId] = useState<string>()
+  // const {
+  //   // isOpen: isAccountReceivableModal,
+  //   // onOpen: onAccountReceivableModalOpen,
+  //   // onClose: onAccountReceivableModalClose,
+  // } = useDisclosure()
+  // const { isOpen: isOpenTransactionModal, onOpen: onEditModalOpen, onClose: onTransactionModalClose } = useDisclosure()
 
-  const onRowClick = useCallback(
-    (_, row) => {
-      if (row.original.type === 'draw') {
-        setSelectedTransactionId(row.original.changeOrderId)
-        setSelectedProjectId(row.original.projectId)
-        onEditModalOpen()
-      } else {
-        setSelectedProjectId(row.original.projectId)
-        onAccountReceivableModalOpen()
-      }
-    },
-    [onAccountReceivableModalOpen],
-  )
-  const { receivableData, isLoading } = usePCRecievable()
-  const receivable = receivableData?.arList
+  // const onRowClick = useCallback(
+  //   (_, row) => {
+  //     if (row.original.type === 'draw') {
+  //       setSelectedTransactionId(row.original.changeOrderId)
+  //       setSelectedProjectId(row.original.projectId)
+  //       onEditModalOpen()
+  //     } else {
+  //       setSelectedProjectId(row.original.projectId)
+  //       onAccountReceivableModalOpen()
+  //     }
+  //   },
+  //   [onAccountReceivableModalOpen],
+  // )
+  // const { receivableData, isLoading } = usePCRecievable()
+  // const receivable = receivableData?.arList
 
-  const [receivableFilterData, setFilterReceivableData] = useState(receivable)
+  // const [receivableFilterData, setFilterReceivableData] = useState(receivable)
 
-  useEffect(() => {
-    if (!selectedCard && !selectedDay) setFilterReceivableData(receivable)
-    setFilterReceivableData(
-      receivable?.filter(
-        project =>
-          !selectedCard || project.durationCategory?.replace(/\s/g, '').toLowerCase() === selectedCard?.toLowerCase(),
-      ),
-    )
+  // useEffect(() => {
+  //   if (!selectedCard && !selectedDay) setFilterReceivableData(receivable)
+  //   setFilterReceivableData(
+  //     receivable?.filter(
+  //       project =>
+  //         !selectedCard || project.durationCategory?.replace(/\s/g, '').toLowerCase() === selectedCard?.toLowerCase(),
+  //     ),
+  //   )
 
-    // Due Project Weekly Filter
-    const getDates = weekDayFilters?.filter(day => selectedDay === day.id)
+  //   // Due Project Weekly Filter
+  //   const getDates = weekDayFilters?.filter(day => selectedDay === day.id)
 
-    const clientDate = getDates?.map(date => {
-      return date?.date
-    })
+  //   const clientDate = getDates?.map(date => {
+  //     return date?.date
+  //   })
 
-    if (selectedDay) {
-      setFilterReceivableData(
-        receivable?.filter(receivableValue => {
-          return clientDate.includes(receivableValue.expectedPaymentDate?.substr(0, 10))
-        }),
-      )
-    }
-  }, [selectedCard, selectedDay, receivable])
+  //   if (selectedDay) {
+  //     setFilterReceivableData(
+  //       receivable?.filter(receivableValue => {
+  //         return clientDate.includes(receivableValue.expectedPaymentDate?.substr(0, 10))
+  //       }),
+  //     )
+  //   }
+  // }, [selectedCard, selectedDay, receivable])
 
   return (
     <Box overflow="auto" width="100%">
-      <TableWrapper
+      {/* <TableWrapper
         onRowClick={onRowClick}
         columns={receivableColumns}
         setTableInstance={setTableInstance}
@@ -130,8 +80,40 @@ export const ReceivableTable: React.FC<ReceivableProps> = ({
         tableHeight="calc(100vh - 300px)"
         name="alerts-table"
         defaultFlexStyle={false}
-      />
-      <AccountReceivableModal
+      /> */}
+      {/* <TableContextProvider
+            data={workOrders}
+            columns={receivableColumns}
+            pagination={pagination}
+            setPagination={setPagination}
+            // columnFilters={columnFilters}
+            setColumnFilters={setColumnFilters}
+            totalPages={totalPages}
+          >
+            <Table isLoading={isLoading} onRowClick={onRowClick} isEmpty={!isLoading && !workOrders?.length} />
+            <TableFooter position="sticky" bottom="0" left="0" right="0">
+              <ButtonsWrapper>
+                <ExportButton
+                  columns={tableColumns}
+                  refetch={refetch}
+                  isLoading={isExportDataLoading}
+                  colorScheme="brand"
+                  fileName="payable.xlsx"
+                />
+                {settingColumns && (
+                  <TableColumnSettings disabled={isLoading} onSave={onSave} columns={settingColumns} />
+                )}
+              </ButtonsWrapper>
+              <TablePagination>
+                <ShowCurrentPageWithTotal />
+                <GotoFirstPage />
+                <GotoPreviousPage />
+                <GotoNextPage />
+                <GotoLastPage />
+              </TablePagination>
+            </TableFooter>
+          </TableContextProvider> */}
+      {/* <AccountReceivableModal
         projectId={selectedProjectId}
         isOpen={isAccountReceivableModal}
         onClose={onAccountReceivableModalClose}
@@ -141,7 +123,7 @@ export const ReceivableTable: React.FC<ReceivableProps> = ({
         onClose={onTransactionModalClose}
         selectedTransactionId={selectedTransactionId as number}
         projectId={selectedProjectId as string}
-      />
+      /> */}
     </Box>
   )
 }
