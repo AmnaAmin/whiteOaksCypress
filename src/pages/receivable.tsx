6de3576a9da5bd49b134dd4c9eeedb28444eea24
Ali Moajiz <moajiznaqvi@gmail.com>
2022-10-31 15:48:45 +0500

    pswoa-99

--- src/pages/receivable.tsx
+++ src/pages/receivable.tsx
@@ -1,19 +1,15 @@
-import { Box, Divider, Flex, FormLabel, Icon, Spacer } from '@chakra-ui/react'
+import { Box, Divider, FormLabel } from '@chakra-ui/react'
 import { DevTool } from '@hookform/devtools'
-import { Button } from 'components/button/button'
 import { ConfirmationBox } from 'components/Confirmation'
 import { ViewLoader } from 'components/page-level-loader'
 import { ReceivableFilter } from 'features/recievable/receivable-filter'
 import { ReceivableTable } from 'features/recievable/receivable-table'
-import { AccountWeekDayFilters } from 'features/common/due-projects-weekly-filter/weekly-filter-accounts-details'
 import { t } from 'i18next'
 import { compact } from 'lodash'
 import { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { BiSync } from 'react-icons/bi'
 import { useBatchProcessingMutation, useCheckBatch } from 'api/account-receivable'
-import { useWeeklyCount } from 'features/payable/hooks'
 import { PaginationState } from '@tanstack/react-table'
+import { useForm } from 'react-hook-form'
 import { PAYABLE_TABLE_QUERY_KEYS } from 'features/payable/payable.constants'
 import { useColumnFiltersQueryString } from 'components/table-refactored/hooks'
 import { useReceivableTableColumns } from 'features/recievable/hook'
@@ -23,12 +19,12 @@ export const Receivable = () => {
   const [loading, setLoading] = useState(false)
   const [isBatchClick, setIsBatchClick] = useState(false)
   const [selectedCard, setSelectedCard] = useState<string>('')
-  const [selectedDay, setSelectedDay] = useState<string>('')
+  const [selectedDay] = useState<string>('')
 
-  const clearAll = () => {
-    setSelectedCard('')
-    setSelectedDay('')
-  }
+  // const clearAll = () => {
+  //   setSelectedCard('')
+  //   setSelectedDay('')
+  // }
 
   const { handleSubmit, register, reset, control, setValue } = useForm()
 
@@ -44,7 +40,7 @@ export const Receivable = () => {
   const { mutate: batchCall } = useBatchProcessingMutation()
   const { refetch } = useCheckBatch(setLoading, loading, queryStringWithPagination, queryStringWithoutPagination)
   const receivableTableColumns = useReceivableTableColumns(control, register, setValue)
-  const { weekDayFilters } = useWeeklyCount()
+  // const { weekDayFilters } = useWeeklyCount()
 
   useEffect(() => {
     if (!loading) {
@@ -89,7 +85,10 @@ export const Receivable = () => {
           <Box mb={2}>
             <ReceivableFilter onSelected={setSelectedCard} cardSelected={selectedCard} />
           </Box>
-          <Flex alignItems="center" py="16px">
+
+          {/* Temp comment */}
+
+          {/* <Flex alignItems="center" py="16px">
             <FormLabel variant="strong-label" size="lg" m="0" pl={2} whiteSpace="nowrap">
               {t('dueProjects')}
             </FormLabel>
@@ -109,7 +108,7 @@ export const Receivable = () => {
               <Icon as={BiSync} fontSize="18px" mr={2} />
               {!loading ? t(`${ACCOUNTS}.batch`) : t(`${ACCOUNTS}.processing`)}
             </Button>
-          </Flex>
+          </Flex> */}
           <Divider border="2px solid #E2E8F0" />
           <Box mt={2} pb="4">
             {loading && <ViewLoader />}
