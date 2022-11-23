d273758e26a3ec58e364438c095d821b2ffae67a
Saqib Hussain <saqibtoori1@gmail.com>
2022-11-23 20:51:50 +0500

    test case issue solved

--- src/mocks/api/project-type/project-type-mocks.ts
+++ src/mocks/api/project-type/project-type-mocks.ts
@@ -3,9 +3,9 @@ export const PROJECT_TYPE_MOCK = [
     id: 43,
     value: 'Other',
     createdBy: 'DevTek',
-    createdDate: '2020-11-01T21:55:24Z',
+    createdDate: '2020-11-01T00:00:00Z',
     lastModifiedBy: 'admin',
-    lastModifiedDate: '2021-04-18T21:17:19Z',
+    lastModifiedDate: '2021-04-18T00:00:00Z',
   },
 ]
 
@@ -13,7 +13,7 @@ export const PROJECT_TYPE_MODAL_MOCK = {
   id: 43,
   value: 'Other',
   createdBy: 'DevTek',
-  createdDate: '2020-11-01T21:55:24Z',
+  createdDate: '2020-11-01T00:00:00Z',
   lastModifiedBy: 'admin',
-  lastModifiedDate: '2021-04-18T21:17:19Z',
+  lastModifiedDate: '2021-04-18T00:00:00Z',
 }
