2818332627307860836c292001bf5d7a13a54980
sania.siddiqui <sania.siddiqui@devtek.ai>
2022-11-25 14:19:25 +0500

    pswoa-787

--- src/features/admin-dashboard/filtered-projects-table.tsx
+++ src/features/admin-dashboard/filtered-projects-table.tsx
@@ -3,11 +3,10 @@ import { Box } from '@chakra-ui/react'
 import { TableContextProvider } from 'components/table-refactored/table-context'
 import Table from 'components/table-refactored/table'
 
-import { PROJECT_COLUMNS } from 'constants/projects.constants'
-import { useFetchFilteredProjects } from 'api/admin-dashboard'
+import { PROJECT_COLUMNS, PROJECT_TABLE_QUERIES_KEY } from 'constants/projects.constants'
 import { ButtonsWrapper, TableFooter } from 'components/table-refactored/table-footer'
 import { ExportButton } from 'components/table-refactored/export-button'
-import { CARD_URL } from './admin-dashboard.utils'
+import { SELECTED_CARD_MAP_URL } from './admin-dashboard.utils'
 import {
   GotoFirstPage,
   GotoLastPage,
@@ -17,6 +16,9 @@ import {
   TablePagination,
 } from 'components/table-refactored/pagination'
 import { useNavigate } from 'react-router-dom'
+import { PaginationState } from '@tanstack/react-table'
+import { useColumnFiltersQueryString } from 'components/table-refactored/hooks'
+import { useGetAllProjects, useProjects } from 'api/projects'
 type ProjectProps = {
   selectedCard: string
 }
@@ -25,13 +27,29 @@ export const FilteredProjectsData: React.FC<ProjectProps> = ({ selectedCard }) =
   const navigate = useNavigate()
   useEffect(() => {
     if (selectedCard) {
-      setFilteredUrl(CARD_URL[selectedCard])
+      setFilteredUrl(SELECTED_CARD_MAP_URL[selectedCard])
+      setPagination({ pageIndex: 0, pageSize: 20 })
     }
   }, [selectedCard])
 
   const [filteredUrl, setFilteredUrl] = useState<string | null>(null)
-  const { data, isLoading, isFetching } = useFetchFilteredProjects(filteredUrl)
+  const [pagination, setPagination] = useState<PaginationState>({ pageIndex: 0, pageSize: 20 })
 
+  const { columnFilters, setColumnFilters, queryStringWithPagination, queryStringWithoutPagination } =
+    useColumnFiltersQueryString({
+      queryStringAPIFilterKeys: PROJECT_TABLE_QUERIES_KEY,
+      pagination,
+      setPagination,
+    })
+  const { projects, isLoading, isFetching, totalPages, dataCount } = useProjects(
+    filteredUrl + '&' + queryStringWithPagination,
+    pagination.pageIndex,
+    pagination.pageSize,
+  )
+
+  const { refetch, isLoading: isExportDataLoading } = useGetAllProjects(
+    filteredUrl + '&' + queryStringWithoutPagination,
+  )
   const onRowClick = rowData => {
     navigate(`/project-details/${rowData.id}`)
   }
@@ -39,24 +57,27 @@ export const FilteredProjectsData: React.FC<ProjectProps> = ({ selectedCard }) =
   return (
     <Box h={'500px'} overflow="auto">
       <TableContextProvider
-        data={data}
+        data={projects}
         columns={PROJECT_COLUMNS}
-        totalPages={data?.length ? Math.ceil(data?.length / 10) : -1}
-        manualPagination={false}
+        pagination={pagination}
+        setPagination={setPagination}
+        columnFilters={columnFilters}
+        setColumnFilters={setColumnFilters}
+        totalPages={totalPages}
       >
-        <Table onRowClick={onRowClick} isLoading={isLoading || isFetching} isEmpty={!isLoading && !data?.length} />
+        <Table onRowClick={onRowClick} isLoading={isLoading || isFetching} isEmpty={!isLoading && !projects?.length} />
         <TableFooter position="sticky" bottom="0" left="0" right="0">
           <ButtonsWrapper>
             <ExportButton
               columns={PROJECT_COLUMNS}
-              fetchedData={data}
-              isLoading={isLoading}
+              refetch={refetch}
+              isLoading={isExportDataLoading}
               colorScheme="brand"
               fileName="projects.xlsx"
             />
           </ButtonsWrapper>
           <TablePagination>
-            <ShowCurrentRecordsWithTotalRecords dataCount={data ? data?.length : 0} />
+            <ShowCurrentRecordsWithTotalRecords dataCount={dataCount} />
             <GotoFirstPage />
             <GotoPreviousPage />
             <GotoNextPage />
