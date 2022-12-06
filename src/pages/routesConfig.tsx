adcb21d2aa8fd5bdb6aafdb9e1c2fd1b1496c92e
haider-sarfraz <m.haidersarfraz@gmail.com>
2022-12-06 14:49:15 +0500

    remove unused import

--- src/pages/routesConfig.tsx
+++ src/pages/routesConfig.tsx
@@ -31,7 +31,6 @@ export default function useRoutesConfig() {
     isVendorManager,
     isDoc,
     isAccounting,
-    isAdmin,
     isOperations,
     isClientManager,
     isConstructionOperations,
