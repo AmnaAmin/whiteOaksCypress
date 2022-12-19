0689ffde921815c2f92de97f31edc2144a0e7bca
yusraadil <yusraadil9a@gmail.com>
2022-12-19 21:54:35 +0500

    Add font to headers

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -23,6 +23,7 @@ export const theme = extendTheme({
   fonts: {
     ...chakraTheme.fonts,
     body: 'Poppins',
+    heading: 'Poppins',
     // body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
     // heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
   },
