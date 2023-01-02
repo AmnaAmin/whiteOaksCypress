2b71e2854c9f7083b8d08a622766e25797429f1b
yusraadil <yusraadil9a@gmail.com>
2023-01-02 21:39:06 +0500

    Fixed table

--- src/features/project-details/audit-logs/audit-logs-table.tsx
+++ src/features/project-details/audit-logs/audit-logs-table.tsx
@@ -7,14 +7,22 @@ import { AUDIT_LOGS_COLUMNS } from './audit-logs.constants'
 import { useProjectAuditLogs } from 'api/project-details'
 
 export const AuditLogsTable = React.forwardRef((_, ref) => {
-  const { projectId } = useParams<'projectId'>()
   const { data: auditLogs, isFetching } = useProjectAuditLogs()
   const onRowClick = row => {}
-console.log('audit/...' , auditLogs)
+  console.log('audit/...', auditLogs)
 
   return (
     <Box>
-      <Box overflow={'auto'} w="100%" h="calc(100vh - 350px)" position="relative" roundedTop={6}>
+      <Box
+        w="100%"
+        minH="calc(100vh - 450px)"
+        position="relative"
+        borderRadius="6px"
+        border="1px solid #CBD5E0"
+        overflowX="auto"
+        roundedRight={{ base: '0px', sm: '6px' }}
+      >
+        {/* <Box overflow={'auto'} w="100%" h="calc(100vh - 350px)" position="relative" roundedTop={6}> */}
         <TableContextProvider data={auditLogs} columns={AUDIT_LOGS_COLUMNS}>
           <Table isLoading={isFetching} isEmpty={!isFetching && !auditLogs?.length} onRowClick={onRowClick} />
         </TableContextProvider>
