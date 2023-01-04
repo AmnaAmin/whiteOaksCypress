bbc76e2b40aafeb559431c1c5956e6601e49d7c5
yusraadil <yusraadil9a@gmail.com>
2023-01-04 14:49:23 +0500

    Added export and settings

--- src/features/project-details/audit-logs/audit-logs-table.tsx
+++ src/features/project-details/audit-logs/audit-logs-table.tsx
@@ -4,11 +4,22 @@ import { TableContextProvider } from 'components/table-refactored/table-context'
 import Table from 'components/table-refactored/table'
 import { AUDIT_LOGS_COLUMNS } from './audit-logs.constants'
 import { useProjectAuditLogs } from 'api/project-details'
+import { ExportButton } from 'components/table-refactored/export-button'
+import { TableFooter, ButtonsWrapper, CustomDivider } from 'components/table-refactored/table-footer'
+import { useTableColumnSettings, useTableColumnSettingsUpdateMutation } from 'api/table-column-settings-refactored'
+import { TableNames } from 'types/table-column.types'
+import TableColumnSettings from 'components/table/table-column-settings'
 
 export const AuditLogsTable = React.forwardRef((_, ref) => {
-  const { data: auditLogs, isFetching } = useProjectAuditLogs()
+  const { data: auditLogs, isFetching, refetch } = useProjectAuditLogs()
+  const { mutate: postGridColumn } = useTableColumnSettingsUpdateMutation(TableNames.auditLogs)
+  const { tableColumns, settingColumns } = useTableColumnSettings(AUDIT_LOGS_COLUMNS, TableNames.auditLogs)
+
   const onRowClick = row => {}
-  console.log('audit/...', auditLogs)
+
+  const onSave = columns => {
+    postGridColumn(columns)
+  }
 
   return (
     <Box>
@@ -23,6 +34,19 @@ export const AuditLogsTable = React.forwardRef((_, ref) => {
       >
         <TableContextProvider data={auditLogs} columns={AUDIT_LOGS_COLUMNS}>
           <Table isLoading={isFetching} isEmpty={!isFetching && !auditLogs?.length} onRowClick={onRowClick} />
+          <TableFooter position="sticky" bottom="0" left="0" right="0">
+            <ButtonsWrapper>
+              <ExportButton
+                columns={tableColumns}
+                refetch={refetch}
+                isLoading={isFetching}
+                colorScheme="darkPrimary.400"
+                fileName="transactions"
+              />
+              <CustomDivider />
+              {settingColumns && <TableColumnSettings disabled={isFetching} onSave={onSave} columns={settingColumns} />}
+            </ButtonsWrapper>
+          </TableFooter>
         </TableContextProvider>
       </Box>
     </Box>
