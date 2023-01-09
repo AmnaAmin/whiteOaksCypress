8982cdd0dd1b50aa0b623a56cfc8323f12bdc4c5
yusraadil <yusraadil9a@gmail.com>
2023-01-09 18:59:39 +0500

    Payment term not being saved after change

--- src/features/project-details/transactions/transaction-form.tsx
+++ src/features/project-details/transactions/transaction-form.tsx
@@ -259,7 +259,7 @@ export const TransactionForm: React.FC<TransactionFormProps> = ({
   useLienWaiverFormValues(control, selectedWorkOrder, setValue)
 
   useEffect(() => {
-    if (selectedWorkOrder?.awardPlanPayTerm) {
+    if (selectedWorkOrder?.awardPlanPayTerm && !transaction?.id) {
       const paymentTermValue = {
         value: selectedWorkOrder?.awardPlanPayTerm,
         label: selectedWorkOrder?.awardPlanPayTerm as string,
@@ -267,7 +267,7 @@ export const TransactionForm: React.FC<TransactionFormProps> = ({
       }
       setValue('paymentTerm', paymentTermValue)
     } else {
-      setValue('paymentTerm', null)
+      setValue('paymentTerm', transaction?.paymentTerm as any)
     }
   }, [selectedWorkOrder])
 
@@ -284,6 +284,11 @@ export const TransactionForm: React.FC<TransactionFormProps> = ({
     resetExpectedCompletionDateFields(option)
   }
 
+  const onPaymentTermChange = (option) => {
+   console.log('paymentTerm', option)
+    setValue('paymentTerm', option)
+  }
+
   const onSubmit = useCallback(
     async (values: FormValues) => {
       const queryOptions = {
@@ -292,6 +297,7 @@ export const TransactionForm: React.FC<TransactionFormProps> = ({
           reset()
         },
       }
+      console.log('paymentTerm', values)
 
       // In case of id exists in transaction object it will be update call to save transaction.
       if (transaction?.id) {
@@ -583,6 +589,7 @@ export const TransactionForm: React.FC<TransactionFormProps> = ({
                                 options={PAYMENT_TERMS_OPTIONS}
                                 isDisabled={isUpdateForm}
                                 onChange={paymentTermOption => {
+                                  onPaymentTermChange(paymentTermOption)
                                   field.onChange(paymentTermOption)
                                 }}
                               />
