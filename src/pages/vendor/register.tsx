4b5db16adfc181e30b28ce56752f08da96f09b92
mun3b <79293820+mun3b@users.noreply.github.com>
2023-01-24 08:09:23 -0600

    Fix: Added Capacity Limit of 500

--- src/pages/vendor/register.tsx
+++ src/pages/vendor/register.tsx
@@ -133,7 +133,7 @@ const vendorRegisterFormSchema = {
   city: Yup.string().required('City is required'),
   state: Yup.object().required('State is required'),
   zipCode: Yup.string().required('ZipCode is required'),
-  capacity: Yup.string().required('Capacity is required').matches(/^\d+$/, 'Must be a digit'),
+  capacity: Yup.number().required('Capacity is required').max(500, "Max Limit is 500"),
 
   //Documents
 
@@ -1125,20 +1125,23 @@ export const VendorRegister = () => {
                                     marginTop: '20px !important',
                                   },
                                 }}
+                                h="69px"
                               >
                                 <FormLabel htmlFor="capacity" sx={formLabeStyle}>
                                   Capacity
                                 </FormLabel>
                                 <Input
                                   id="capacity"
-                                  type="text"
+                                  type="number"
                                   fontSize="14px"
                                   color="#252F40"
                                   placeholder="Please enter your capacity"
                                   {...register('capacity', {
                                     required: 'This is required',
+                                    max: 500,
                                   })}
                                   variant="required-field"
+                                  max="500"
                                 />
                                 <FormErrorMessage>{errors?.capacity && errors?.capacity?.message}</FormErrorMessage>
                               </FormControl>
