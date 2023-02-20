0f20aac1b2fe7ab6c556d08525432b6eccf965fc
Muhammad Sarwar <hafiz_m_sarwar@elixir.com>
2023-02-20 13:17:34 +0500

    pswoa-2059

--- src/pages/estimate-details.tsx
+++ src/pages/estimate-details.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import { useEffect, useRef } from 'react'
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from 'utils/auth-context';
 import { getToken } from 'utils/storage.utils'
 
@@ -30,7 +30,7 @@ export const EstimateDetails = () => {
     );
 
   }
-
+  const navigate = useNavigate();
   useEffect( () => {
 
     window.addEventListener( "message", ( event: any ) => {
@@ -42,6 +42,13 @@ export const EstimateDetails = () => {
       if ( event.data.isEstimatesDetailsPage ) {
         //window.history.pushState(null, '', `/${event.data.path}/${event.data.estimatesId}`);
       }
+      if ( event.data.redirectToProjectsPage ) {
+        setTimeout(()=> {
+          window.location.href = `project-details/${event.data.projectId}/`;
+        }, 300)
+        console.log(`project-details/${event.data.projectId}`);
+      }
+
       console.log( "WOA: Event Data Received From Child", event );
     } );
 
