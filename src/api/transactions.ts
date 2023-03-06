a759bb09526c0b49ff48687e3bcad95dfe8a74f3
Ali Moajiz <moajiznaqvi@gmail.com>
2023-03-06 22:46:46 +0500

    pswoa-added-incorportions

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -62,8 +62,6 @@ export const useTransactions = (projectId?: string) => {
 
 export const useTransactionsV1 = (projectId?: string) => {
   const client = useClient()
-  console.log(projectId)
-  console.log(!!projectId)
 
   const { data: transactions, ...rest } = useQuery<Array<TransactionType>>(
     [GET_TRANSACTIONS_API_KEY, projectId],
@@ -165,7 +163,7 @@ const transactionTypeOptions = [
   {
     value: TransactionTypeValues.depreciation,
     label: 'Depreciation',
-  },
+  }
 ]
 
 export const useTransactionTypes = (screen?: string) => {
