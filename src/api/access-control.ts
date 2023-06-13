d6a6a7a9186c2ab00fa7cf904709c034491eb0a4
sania.siddiqui <sania.siddiqui@devtek.ai>
2023-06-13 22:45:53 +0500

    pswoa-3363

--- src/api/access-control.ts
+++ src/api/access-control.ts
@@ -142,3 +142,45 @@ export const ASSIGNMENTS = [
   { value: 'FPM', label: 'FPM' },
   { value: 'PC', label: 'Project Coordinator' },
 ]
+
+export const mapPermissionsToFormValues = permissionsArray => {
+  const permissions = permissionsArray
+    ?.filter(p => {
+      // select all permissions that are at module level like VENDOR.EDIT, PROJECT.EDIT
+      const permissionKey = p.key
+      const splitPermission = permissionKey.split('.')
+      return splitPermission?.length === 2
+    })
+    ?.map(p => {
+      const permissionKey = p.key
+      const splitPermission = permissionKey.split('.')
+      return { section: splitPermission?.[0], action: splitPermission?.[splitPermission?.length - 1] }
+    })
+
+  const sectionWisePermissions = [] as any
+  SECTIONS?.forEach(s => {
+    const permissionObj = permissions?.find(p => s.value === p.section && ['READ', 'EDIT'].includes(p.action))
+    sectionWisePermissions?.push({
+      name: s.value,
+      edit: permissionObj?.action === 'EDIT',
+      read: permissionObj?.action === 'READ',
+      hide: !permissionObj,
+    })
+  })
+  return sectionWisePermissions
+}
+
+export const mapFormValuestoPayload = (values, allPermissions) => {
+  const permissions = values.permissions
+    ?.filter(p => !p.hide && (p.edit || p.read)) //remove all hidden. And select ones that have edit or read true
+    ?.map(p => {
+      const key = p.name + '.' + (p.edit ? 'EDIT' : 'READ')
+      return allPermissions?.find(a => a.key === key)
+    })
+  return {
+    name: values?.roleName,
+    location: values?.location?.value,
+    assignment: values?.assignment?.value,
+    permissions,
+  }
+}
--- src/features/access-control/roles-permissions.tsx
+++ src/features/access-control/roles-permissions.tsx
@@ -26,7 +26,8 @@ import { useEffect } from 'react'
 import {
   ASSIGNMENTS,
   LOCATIONS,
-  SECTIONS,
+  mapFormValuestoPayload,
+  mapPermissionsToFormValues,
   useCreateNewRoleMutation,
   useFetchAllPermissions,
   useUpdateRoleMutation,
@@ -63,49 +64,8 @@ export const RolesPermissions = ({ permissions, setNewRole, setSelectedRole }) =
     }
   }
 
-  const mapPermissionsToFormValues = permissionsArray => {
-    const permissions = permissionsArray
-      ?.filter(p => {
-        // select all permissions that are at module level like VENDOR.EDIT, PROJECT.EDIT
-        const permissionKey = p.key
-        const splitPermission = permissionKey.split('.')
-        return splitPermission?.length === 2
-      })
-      ?.map(p => {
-        const permissionKey = p.key
-        const splitPermission = permissionKey.split('.')
-        return { section: splitPermission?.[0], action: splitPermission?.[splitPermission?.length - 1] }
-      })
-
-    const sectionWisePermissions = [] as any
-    SECTIONS?.forEach(s => {
-      const permissionObj = permissions?.find(p => s.value === p.section && ['READ', 'EDIT'].includes(p.action))
-      sectionWisePermissions?.push({
-        name: s.value,
-        edit: permissionObj?.action === 'EDIT',
-        read: permissionObj?.action === 'READ',
-        hide: !permissionObj,
-      })
-    })
-    return sectionWisePermissions
-  }
-
-  const mapFormValuestoPayload = values => {
-    const permissions = values.permissions
-      ?.filter(p => !p.hide && (p.edit || p.read)) //remove all hidden. And select ones that have edit or read true
-      ?.map(p => {
-        const key = p.name + '.' + (p.edit ? 'EDIT' : 'READ')
-        return allPermissions?.find(a => a.key === key)
-      })
-    return {
-      name: values?.roleName,
-      location: values?.location?.value,
-      assignment: values?.assignment?.value,
-      permissions,
-    }
-  }
   const onSubmit = values => {
-    let payload = mapFormValuestoPayload(values)
+    let payload = mapFormValuestoPayload(values, allPermissions)
     if (!permissions) {
       createRole(payload as any, {
         onSuccess: () => {
