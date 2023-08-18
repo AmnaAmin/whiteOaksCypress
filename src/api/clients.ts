56f105847668b715dfc3270c1b98811a00b28f1b
Arzoo Khalil <arzoo.khalil@devtek.ai>
2023-08-18 13:46:35 +0500

    Client-table.tsx: 1.Revamp Export Button Functionality

--- src/api/clients.ts
+++ src/api/clients.ts
@@ -10,8 +10,6 @@ import { useClient } from 'utils/auth-context'
 import { usePaginationQuery } from 'api'
 import { reduceArrayToObject } from 'utils'
 
-
-
 const getClientQueryString = (filterQueryString: string) => {
   let queryString = filterQueryString
   if (filterQueryString?.search('&sort=id.equals') < 0) {
@@ -35,6 +33,23 @@ export const useClients = (queryString: string = '', pageSize: number = 20) => {
     dataCount: data?.dataCount,
     ...rest,
   }
+}
+
+const GET_ALL_CLIENT_QUERY_KEY = 'all_clients'
+
+export const useGetAllClients = (queryString: string) => {
+  const client = useClient()
+  const apiQueryString = getClientQueryString(queryString)
+  return useQuery(
+    [GET_ALL_CLIENT_QUERY_KEY, apiQueryString],
+    async () => {
+      const response = await client(`clients?${apiQueryString}`, {})
+      return response?.data
+    },
+    {
+      enabled: false,
+    },
+  )
 }
 
 export const useNotes = ({ clientId }: { clientId: number | undefined }) => {
@@ -273,7 +288,7 @@ export const useSubFormErrors = (errors: FieldErrors<ClientFormValues>) => {
 }
 
 export const mappingDataForClientExport = (data, columns) => {
-  return data.map((row: any) => {
+  return data?.map((row: any) => {
     const columnDefWithAccessorKeyAsKey = reduceArrayToObject(columns, 'accessorKey')
     return Object.keys(columnDefWithAccessorKeyAsKey).reduce((acc, key) => {
       let value = ''
@@ -316,4 +331,4 @@ export const CLIENT_TABLE_QUERY_KEYS = {
   accountPayableContactInfosContact: 'accountPayableContactInfosContact.contains',
   accountPayableContactInfosEmail: 'accountPayableContactInfosEmail.contains',
   accountPayableContactInfosPhone: 'accountPayableContactInfosPhone.contains',
-};
+}
