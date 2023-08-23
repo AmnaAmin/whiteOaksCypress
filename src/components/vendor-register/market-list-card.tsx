eadd1958e24babe0e92da217e983ef35549b8ce6
SobiaGerdazi <Sobia.Gerdazi@DevTek.ai>
2023-08-23 18:33:16 +0500

    ids for cypress

--- src/components/vendor-register/market-list-card.tsx
+++ src/components/vendor-register/market-list-card.tsx
@@ -27,7 +27,7 @@ export const MarketForm = ({ onClose, isActive, markets }: marketFormProps) => {
   return (
     <>
       <Box h="584px" overflow="auto">
-        <Flex maxW="800px" wrap="wrap" gridGap={3} position={'relative'}>
+        <Flex id='market_vendor' maxW="800px" wrap="wrap" gridGap={3} position={'relative'}>
           {tradeCheckboxes?.map((checkbox, index) => {
             return (
               <Controller
