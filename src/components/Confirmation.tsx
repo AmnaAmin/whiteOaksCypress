99232d402a27bb788db2d070d98b90765a3cc79b
SobiaGerdazi <Sobia.Gerdazi@DevTek.ai>
2023-09-19 12:49:13 +0500

    fix text font weight

--- src/components/Confirmation.tsx
+++ src/components/Confirmation.tsx
@@ -54,7 +54,7 @@ export function ConfirmationBox({
       <ModalContent rounded="6">
         <ModalHeader
           borderBottom="2px solid #E2E8F0"
-          fontWeight={400}
+          fontWeight={500}
           color="gray.600"
           fontSize="16px"
           fontStyle="normal"
