050b9f2e5b7937c1806dcd6fe40735f41fa3518a
Arzoo Khalil <arzoo.khalil@devtek.ai>
2023-09-26 17:53:49 +0500

    remove build issue

--- src/components/table-refactored/table.tsx
+++ src/components/table-refactored/table.tsx
@@ -67,8 +67,14 @@ function Filter({
   )
 
   const [selectionRange, setSelectionRange] = useState({
-    startDate: new Date(),
-    endDate: new Date(),
+    startDate:
+      dateFilter && columnFilterValue && columnFilterValue !== undefined && columnFilterValue !== ' - '
+        ? new Date(columnFilterValue.split(' - ')[0]?.toString())
+        : new Date(),
+    endDate:
+      dateFilter && columnFilterValue && columnFilterValue !== undefined && columnFilterValue !== ' - '
+        ? new Date(columnFilterValue.split(' - ')[1]?.toString())
+        : new Date(),
     key: 'selection',
   })
 
