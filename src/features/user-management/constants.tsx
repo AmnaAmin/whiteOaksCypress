7d1607ba14dcdca844d6181f8dbd95a41dd99523
Haroon Khan <haroon.khan@devtek.ai>
2023-10-05 15:47:35 +0500

    fix the user grid account filtering with contains

--- src/features/user-management/constants.tsx
+++ src/features/user-management/constants.tsx
@@ -88,7 +88,7 @@ export const USER_MGT_TABLE_QUERIES_KEY = {
   email: 'email.contains',
   firstName: 'firstName.contains',
   lastName: 'lastName.contains',
-  authorities: 'authorities.in',
+  authorities: 'authorities.contains',
   langKey: 'langKey.contains',
   activatedLabel: 'activatedLabel.contains',
   createdDateStart: 'createdDate.greaterThanOrEqual',
