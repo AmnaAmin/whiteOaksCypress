33455173c008540baa22149fe47fa8310a54658f
Arzoo Khalil <arzoo.khalil@devtek.ai>
2023-10-15 01:18:22 +0500

    Commit test cases

--- src/features/work-order/__test__/work-order-lien-waiver-tab.spec.tsx
+++ src/features/work-order/__test__/work-order-lien-waiver-tab.spec.tsx
@@ -90,42 +90,42 @@ describe('Work Order Lien Waiver Test Cases', () => {
     expect(screen.queryByTestId('save-lien-waiver')).not.toBeInTheDocument()
   })
 
-//   test('User is able to view readonly info in lien waiver. User can enter & save claimants title & signature again if LW has been rejected. An alert info regarding rejection is also displayed', async () => {
-//     const onClose = jest.fn()
-//     const workOrder = WORK_ORDERS.find(w => w.leanWaiverSubmitted && !w.lienWaiverAccepted)
-//     const documentsData = DOCUMENTS
-//     await renderLienWaiver({ onClose, workOrder, documentsData })
+  //   test('User is able to view readonly info in lien waiver. User can enter & save claimants title & signature again if LW has been rejected. An alert info regarding rejection is also displayed', async () => {
+  //     const onClose = jest.fn()
+  //     const workOrder = WORK_ORDERS.find(w => w.leanWaiverSubmitted && !w.lienWaiverAccepted)
+  //     const documentsData = DOCUMENTS
+  //     await renderLienWaiver({ onClose, workOrder, documentsData })
 
-//     // Check Alert Info is available
-//     expect(screen.getByTestId('lienWaiverRejectInfo')).toBeInTheDocument()
+  //     // Check Alert Info is available
+  //     expect(screen.getByTestId('lienWaiverRejectInfo')).toBeInTheDocument()
 
-//     // Check Claimant Title, Signature Fields and Save is enabled
-//     expect(screen.getByTestId('claimantsTitle')).not.toHaveAttribute('disabled')
-//     expect(screen.getByTestId('save-lien-waiver')).toBeInTheDocument()
-//     expect(screen.queryByTestId('recentLW')).not.toBeInTheDocument()
+  //     // Check Claimant Title, Signature Fields and Save is enabled
+  //     expect(screen.getByTestId('claimantsTitle')).not.toHaveAttribute('disabled')
+  //     expect(screen.getByTestId('save-lien-waiver')).toBeInTheDocument()
+  //     expect(screen.queryByTestId('recentLW')).not.toBeInTheDocument()
 
-//     // Enter Claimant Title
-//     userEvent.type(screen.getByTestId('claimantsTitle'), 'SSiddiqui')
-//     expect((screen.getByTestId('claimantsTitle') as HTMLInputElement).value).toEqual('SSiddiqui')
+  //     // Enter Claimant Title
+  //     userEvent.type(screen.getByTestId('claimantsTitle'), 'SSiddiqui')
+  //     expect((screen.getByTestId('claimantsTitle') as HTMLInputElement).value).toEqual('SSiddiqui')
 
-//     // Enter Signature
-//     await act(async () => await userEvent.click(screen.getByLabelText('open-signature')))
-//     expect(screen.getByTestId('signature-input')).toBeInTheDocument()
+  //     // Enter Signature
+  //     await act(async () => await userEvent.click(screen.getByLabelText('open-signature')))
+  //     expect(screen.getByTestId('signature-input')).toBeInTheDocument()
 
-//     userEvent.type(screen.getByTestId('signature-input'), 'Siddiqui')
-//     expect((screen.getByTestId('signature-input') as HTMLInputElement).value).toEqual('Siddiqui')
+  //     userEvent.type(screen.getByTestId('signature-input'), 'Siddiqui')
+  //     expect((screen.getByTestId('signature-input') as HTMLInputElement).value).toEqual('Siddiqui')
 
-//     await act(async () => await userEvent.click(screen.getByTestId('save-signature')))
+  //     await act(async () => await userEvent.click(screen.getByTestId('save-signature')))
 
-//     // Check Signature date rendered properly
-//     expect((screen.getByTestId('signature-date') as HTMLInputElement).value).toEqual(dateFormat(new Date()))
+  //     // Check Signature date rendered properly
+  //     expect((screen.getByTestId('signature-date') as HTMLInputElement).value).toEqual(dateFormat(new Date()))
 
-//     // Save Lien Waiver and confirmation box shows
-//     await act(async () => await userEvent.click(screen.getByTestId('save-lien-waiver')))
-//     expect(screen.queryByTestId('confirmation-message')).toBeInTheDocument()
+  //     // Save Lien Waiver and confirmation box shows
+  //     await act(async () => await userEvent.click(screen.getByTestId('save-lien-waiver')))
+  //     expect(screen.queryByTestId('confirmation-message')).toBeInTheDocument()
 
-//     // Confirm yes will start loading as api is called
-//     await act(async () => await userEvent.click(screen.getByTestId('confirmation-yes')))
-//     expect(screen.queryByText(/Loading/i)).toBeInTheDocument()
-//   })
-// })
+  //     // Confirm yes will start loading as api is called
+  //     await act(async () => await userEvent.click(screen.getByTestId('confirmation-yes')))
+  //     expect(screen.queryByText(/Loading/i)).toBeInTheDocument()
+  //   })
+})
