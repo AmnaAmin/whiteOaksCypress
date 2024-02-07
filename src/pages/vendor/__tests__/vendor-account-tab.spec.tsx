0191bbb95146568656b4b38e94c4c7382db6a154
Hamza Nawab <hamzanawab.bs@gmail.com>
2024-02-07 18:14:44 +0500

    Unit test - PSWOA-9120

--- src/pages/vendor/__tests__/vendor-account-tab.spec.tsx
+++ src/pages/vendor/__tests__/vendor-account-tab.spec.tsx
@@ -3,13 +3,14 @@ import App from 'App'
 import { VendorProfileTabs } from '../vendor-profile'
 import { Providers } from 'providers'
 import { BrowserRouter } from 'react-router-dom'
+import { VENDOR } from 'mocks/api/vendor-profile/data'
 
 const onClose = jest.fn()
 
 const renderVendorProfileModal = async () => {
     render(
         <BrowserRouter>
-            <VendorProfileTabs onClose={onClose} />
+            <VendorProfileTabs vendorProfileData={VENDOR} vendorId={VENDOR.id} onClose={onClose} />
         </BrowserRouter>
         , { wrapper: Providers })
 
@@ -20,6 +21,16 @@ const renderVendorProfileModal = async () => {
 describe("Vendor Tab Account Test Cases", () => {
     test('Should render Vendor Modal', async () => {
         await renderVendorProfileModal()
+        screen.debug(undefined, 1000000000000000000000000000)
+        // Unit test of tabs of vendor modal exists
+        expect(screen.getByText("Details")).toBeInTheDocument()
+        expect(screen.getByText("Documents")).toBeInTheDocument()
+        expect(screen.getByText("License")).toBeInTheDocument()
+        expect(screen.getByText("Skill")).toBeInTheDocument()
+        expect(screen.getByText("Market")).toBeInTheDocument()
+        expect(screen.getByText("Projects")).toBeInTheDocument()
+        expect(screen.getByText("Users")).toBeInTheDocument()
+        expect(screen.getByText("Messages")).toBeInTheDocument()
     })
 
 })
