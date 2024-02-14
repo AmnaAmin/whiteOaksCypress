e106897df83a31958f89a84544c052c83a582834
Mu33b <imuneeb@me.com>
2024-02-14 01:17:42 -0600

    Invoice Number- Implement updates on invoice
    Need to do the following in invoice number
    
    Comment out code for revision invoice
    
    Restriction on duplicate invoice numbers in project
    
    Code level change.
    Need to comment out code of revision of invoice number functionality
    
    This functionality needs to be removed
    
    We need to keep the code for enabling it in the future, if needed
    
    The rest functionality should not be impacted.
    
    Just on any updating on the invoice should not update the invoice number to R1 ,R2 etc
    
    This functionality should remain as is When a user creates the first invoice, there will be an additional 01.
    
    Restriction
    Need to add restriction to duplicate invoice number.
    
    User should never be able to add manually an invoice number that already exists in the project.
    
    If user has manually added an invoice number and then on another invoice again tries to add same invoice number to any other invoice in the project, it should give error
    
    If an invoice number is autogenerated, the user should not be able to add that number manually to any other invoice.

--- src/api/invoicing.ts
+++ src/api/invoicing.ts
@@ -97,85 +97,67 @@ export const useUpdateInvoiceMutation = ({ projId }) => {
   const queryClient = useQueryClient()
   const { projectId } = useParams<'projectId'>() || projId
 
-  function updateInvoiceNumberByRevision(invoiceNo: string): string {
-    const revisionPattern = new RegExp(/(\d+)-R(\d+)$/)
-    const matchResult = revisionPattern.exec(invoiceNo)
+  // function updateInvoiceNumberByRevision(invoiceNo: string): string {
+  //   const revisionPattern = new RegExp(/(\d+)-R(\d+)$/)
+  //   const matchResult = revisionPattern.exec(invoiceNo)
 
-    if (matchResult) {
-      const newRevision = parseInt(matchResult[2]) + 1
-      return invoiceNo.replace(revisionPattern, `$1-R${newRevision}`)
-    } else {
-      // If no match at the end, add '-R1' to the end
-      return `${invoiceNo}-R1`
-    }
-  }
-  function compareInvoiceLineItems(payloadLineItems: [], invoiceDetailsLineItems: []) {
-    if (payloadLineItems.length !== invoiceDetailsLineItems.length) {
-      return false
-    }
+  //   if (matchResult) {
+  //     const newRevision = parseInt(matchResult[2]) + 1
+  //     return invoiceNo.replace(revisionPattern, `$1-R${newRevision}`)
+  //   } else {
+  //     // If no match at the end, add '-R1' to the end
+  //     return `${invoiceNo}-R1`
+  //   }
+  // }
+  // function compareInvoiceLineItems(payloadLineItems: [], invoiceDetailsLineItems: []) {
+  //   if (payloadLineItems.length !== invoiceDetailsLineItems.length) {
+  //     return false
+  //   }
 
-    for (let i = 0; i < payloadLineItems.length; i++) {
-      const payloadItemKeys = Object.keys(payloadLineItems[i])
+  //   for (let i = 0; i < payloadLineItems.length; i++) {
+  //     const payloadItemKeys = Object.keys(payloadLineItems[i])
 
-      for (const key of payloadItemKeys) {
-        if (payloadLineItems[i][key] !== invoiceDetailsLineItems[i][key]) {
-          return false
-        }
-      }
-    }
-    return true
-  }
+  //     for (const key of payloadItemKeys) {
+  //       if (payloadLineItems[i][key] !== invoiceDetailsLineItems[i][key]) {
+  //         return false
+  //       }
+  //     }
+  //   }
+  //   return true
+  // }
 
   return useMutation(
     async (payload: any) => {
-      const responseInvoiceDetails = await client(`project-invoices/${payload.id}`, {})
-      const invoiceDetails = responseInvoiceDetails?.data as InvoicingType
-      let isInvoiceChanged = false
-      if (payload.paymentTerm !== invoiceDetails.paymentTerm) {
-        console.log('Invoice name changed reason: payment term')
-        isInvoiceChanged = true
-      }
-      if (payload.invoiceDate !== invoiceDetails.invoiceDate) {
-        console.log('Invoice name changed reason: invoice date')
-        isInvoiceChanged = true
-      }
-      // if ( !!payload.receivedLineItems?.length && !!invoiceDetails.receivedLineItems?.length && ( payload.receivedLineItems.length !== invoiceDetails.receivedLineItems.length )  ) isInvoiceChanged = true;
-      // if ( !!payload.invoiceLineItems?.length && !!invoiceDetails.invoiceLineItems?.length && ( payload.invoiceLineItems.length !== invoiceDetails.invoiceLineItems.length )  ) isInvoiceChanged = true;
+      // const responseInvoiceDetails = await client(`project-invoices/${payload.id}`, {})
+      // const invoiceDetails = responseInvoiceDetails?.data as InvoicingType
+      // let isInvoiceChanged = false
+      // if (payload.paymentTerm !== invoiceDetails.paymentTerm) {
+      //   console.log('Invoice name changed reason: payment term')
+      //   isInvoiceChanged = true
+      // }
+      // if (payload.invoiceDate !== invoiceDetails.invoiceDate) {
+      //   console.log('Invoice name changed reason: invoice date')
+      //   isInvoiceChanged = true
+      // }
+      
+      // if (
+      //   !compareInvoiceLineItems(
+      //     payload.invoiceLineItems?.filter(l => l.type === 'finalSowLineItems'),
+      //     invoiceDetails.invoiceLineItems,
+      //   )
+      // ) {
+      //   console.log('Invoice name changed reason: invoice line items')
+      //   isInvoiceChanged = true
+      // }
 
-      //compare length and each single property value
-      if (
-        !compareInvoiceLineItems(
-          payload.invoiceLineItems?.filter(l => l.type === 'finalSowLineItems'),
-          invoiceDetails.invoiceLineItems,
-        )
-      ) {
-        console.log('Invoice name changed reason: invoice line items')
-        isInvoiceChanged = true
-      }
-
-      console.log('Payload: ', payload)
-      console.log('Details: ', invoiceDetails)
-
-      if (isInvoiceChanged) {
-        const pattern = /-(\d+)(?:-R(\d+))?$/
-        //const revisionPattern = new RegExp(/^(\d+)-R(\d+)/);
-        const currInvoiceNumber = payload.invoiceNumber
-        const match = currInvoiceNumber.match(pattern)
-
-        // console.log("New number: ", updateInvoiceNumberByRevision(currInvoiceNumber))
-        // console.log("revisions", revisionPattern.exec(currInvoiceNumber));
-
-        if (match) {
-          // const incrementedNumber = parseInt(match[1]) + 1;
-          // const incrementedString = (incrementedNumber < 10 ? "0" : "") + incrementedNumber;
-          // payload.invoiceNumber =  currInvoiceNumber.replace(pattern, incrementedString + "-");
-
-          payload.invoiceNumber = updateInvoiceNumberByRevision(currInvoiceNumber)
-        }
-      }
-
-      // console.log(payload); return;
-
+      // if (isInvoiceChanged) {
+      //   const pattern = /-(\d+)(?:-R(\d+))?$/
+      //   const currInvoiceNumber = payload.invoiceNumber
+      //   const match = currInvoiceNumber.match(pattern)
+      //   if (match) {
+      //     payload.invoiceNumber = updateInvoiceNumberByRevision(currInvoiceNumber)
+      //   }
+      // }
       return client('project-invoices', {
         data: payload,
         method: 'PUT',
