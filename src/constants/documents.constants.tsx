6c4ff4a0668b1ba860f8d3e491ff79b8f7370491
Moajiz1 <moajiznaqvi@gmail.com>
2024-02-29 15:53:33 +0500

    pswoa-6136

--- src/constants/documents.constants.tsx
+++ src/constants/documents.constants.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { ColumnDef } from '@tanstack/react-table'
+import React, { useMemo } from 'react'
 import { BiDownArrowCircle } from 'react-icons/bi'
 import { Text, Flex, Box, Icon, Spacer } from '@chakra-ui/react'
 import { downloadFileOnly } from 'utils/file-utils'
@@ -19,193 +18,208 @@ const withPreviewCell = cellInfo => {
   return <Text onClick={() => window.open(s3Url, '_blank')}>{value}</Text>
 }
 
-export const DOCUMENTS_TABLE_COLUMNS: ColumnDef<any>[] = [
-  {
-    header: '',
-    id: 'expander',
-    size: 40,
-    cell: ({ row, getValue }) => (
-      <Flex
-        onClick={e => e.stopPropagation()}
-        style={{
-          paddingLeft: `${row.depth * 1.5}rem`,
-        }}
-      >
-        {row.getToggleExpandedHandler()}
-        <>
-          {row.getCanExpand() ? (
-            <button
-              data-testid="expension-&-compression-btn"
-              {...{
-                onClick: row.getToggleExpandedHandler(),
-                style: { cursor: 'pointer' },
-              }}
-            >
-              {row.getIsExpanded() ? (
-                <DownArrow
-                  style={{
-                    marginRight: '8px',
-                  }}
-                />
-              ) : (
-                <RightArrow
-                  style={{
-                    marginRight: '8px',
-                  }}
-                />
-              )}
-            </button>
-          ) : (
-            ''
-          )}
-          {getValue()}
-        </>
-      </Flex>
-    ),
-    accessorFn: cellInfo => {
-      return (
-        <div style={{ marginTop: '1px' }}>
-          {/* {cellInfo.parentWorkOrderId ? cellInfo.parentWorkOrderId?.toString() : '- - -'} */}
-        </div>
-      )
-    },
-  },
-
-  {
-    id: 'id',
-    header: 'documentId',
-    accessorKey: 'id',
-    accessorFn: cellInfo => {
-      return cellInfo?.id ? cellInfo.id?.toString() : '- - -'
-    },
-    cell: withPreviewCell,
-  },
-
-  {
-    id: 'WorkOrderId',
-    header: 'WorkOrder ID',
-    accessorKey: 'workOrderId',
-    accessorFn: cellInfo => {
-      return cellInfo?.workOrderId ? cellInfo.workOrderId?.toString() : '- - -'
-    },
-    cell: withPreviewCell,
-  },
-
-  {
-    id: 'fileType',
-    header: 'document',
-    accessorKey: 'fileType',
-    cell: withPreviewCell,
-  },
-  {
-    id: 'documentType',
-    header: 'documentType',
-    accessorKey: 'documentTypelabel',
-    cell: withPreviewCell,
-  },
-  {
-    header: 'transactionDoc',
-    accessorKey: 'label',
-    id: 'label',
-    cell: withPreviewCell,
-  },
-  {
-    id: 'vendorName',
-    header: 'vendorGL',
-    accessorKey: 'vendorName',
-    cell: withPreviewCell,
-    filterFn: 'includesString',
-  },
-
-  {
-    id: 'workOrderName',
-    header: 'Skill',
-    accessorKey: 'workOrderName',
-    cell: withPreviewCell,
-    filterFn: 'includesString',
-  },
-  {
-    id: 'fileObjectContentType',
-    header: 'fileType',
-    accessorKey: 'fileObjectContentType',
-    cell: withPreviewCell,
-  },
-
-  {
-    header: 'createdBy',
-    accessorKey: 'createdBy',
-    id: 'createdBy',
-    cell: withPreviewCell,
-  },
-  {
-    header: 'createdDate',
-    accessorKey: 'createdDate',
-    id: 'createdDate',
-    accessorFn(cellInfo) {
-      return datePickerFormat(cellInfo.createdDate)
-    },
-
-    meta: { format: 'date' },
-    cell(cellInfo) {
-      return (
-        <Flex position="relative">
-          <Box mr={2}>{dateFormat(cellInfo.getValue() as string)}</Box>
-          <Spacer w="90px" />
-          <Box
-            left="81%"
-            top="-85%"
-            w="39px"
-            h="46px"
-            position="absolute"
-            onClick={() => {
-              downloadFileOnly(cellInfo.row.original)
-            }}
-            _hover={{ cursor: 'pointer', backgroundColor: '#CFEDFD' }}
-          >
-            <Icon
-              p="1px"
-              as={BiDownArrowCircle}
-              color="#345EA6"
-              fontSize={24}
-              position="relative"
-              top="10px"
-              // bottom="0"
-              left="8px"
-              // right="0"
-              // m="auto"
-            />
-          </Box>
-        </Flex>
-      )
-    },
-  },
-
-  {
-    header: '',
-    id: 'deleteBtn',
-    accessorKey: '',
-
-    cell(cellInfo) {
-      return (
-        <Flex ml="-9px" onClick={() => console.log(cellInfo.row.original)}>
-          {/* <Box mr={2}>{dateFormat(cellInfo.getValue() as string)}</Box> */}
-          <DeleteIcon
+export const useGetDocumentsTableColumn = (onResetConfirmationModalOpen: () => void, getDocId: (id) => void) => {
+  let columns = useMemo(() => {
+    return [
+      {
+        header: '',
+        id: 'expander',
+        size: 40,
+        cell: ({ row, getValue }) => (
+          <Flex
+            onClick={e => e.stopPropagation()}
             style={{
-              marginRight: '8px',
-              color: '#345EA6',
-              fontSize: 18,
+              paddingLeft: `${row.depth * 1.5}rem`,
             }}
-          />
-        </Flex>
-      )
-    },
-    accessorFn: () => {
-      return (
-        <div style={{ marginTop: '1px' }}>
-          {/* {cellInfo.parentWorkOrderId ? cellInfo.parentWorkOrderId?.toString() : '- - -'} */}
-        </div>
-      )
-    },
-    size: 20,
-  },
-]
+          >
+            {row.getToggleExpandedHandler()}
+            <>
+              {row.getCanExpand() ? (
+                <button
+                  data-testid="expension-&-compression-btn"
+                  {...{
+                    onClick: row.getToggleExpandedHandler(),
+                    style: { cursor: 'pointer' },
+                  }}
+                >
+                  {row.getIsExpanded() ? (
+                    <DownArrow
+                      style={{
+                        marginRight: '8px',
+                      }}
+                    />
+                  ) : (
+                    <RightArrow
+                      style={{
+                        marginRight: '8px',
+                      }}
+                    />
+                  )}
+                </button>
+              ) : (
+                ''
+              )}
+              {getValue()}
+            </>
+          </Flex>
+        ),
+        accessorFn: cellInfo => {
+          return (
+            <div style={{ marginTop: '1px' }}>
+              {/* {cellInfo.parentWorkOrderId ? cellInfo.parentWorkOrderId?.toString() : '- - -'} */}
+            </div>
+          )
+        },
+      },
+
+      {
+        id: 'id',
+        header: 'documentId',
+        accessorKey: 'id',
+        accessorFn: cellInfo => {
+          return cellInfo?.id ? cellInfo.id?.toString() : '- - -'
+        },
+        cell: withPreviewCell,
+      },
+
+      {
+        id: 'WorkOrderId',
+        header: 'WorkOrder ID',
+        accessorKey: 'workOrderId',
+        accessorFn: cellInfo => {
+          return cellInfo?.workOrderId ? cellInfo.workOrderId?.toString() : '- - -'
+        },
+        cell: withPreviewCell,
+      },
+
+      {
+        id: 'fileType',
+        header: 'document',
+        accessorKey: 'fileType',
+        cell: withPreviewCell,
+      },
+      {
+        id: 'documentType',
+        header: 'documentType',
+        accessorKey: 'documentTypelabel',
+        cell: withPreviewCell,
+      },
+      {
+        header: 'transactionDoc',
+        accessorKey: 'label',
+        id: 'label',
+        cell: withPreviewCell,
+      },
+      {
+        id: 'vendorName',
+        header: 'vendorGL',
+        accessorKey: 'vendorName',
+        cell: withPreviewCell,
+        filterFn: 'includesString',
+      },
+
+      {
+        id: 'workOrderName',
+        header: 'Skill',
+        accessorKey: 'workOrderName',
+        cell: withPreviewCell,
+        filterFn: 'includesString',
+      },
+      {
+        id: 'fileObjectContentType',
+        header: 'fileType',
+        accessorKey: 'fileObjectContentType',
+        cell: withPreviewCell,
+      },
+
+      {
+        header: 'createdBy',
+        accessorKey: 'createdBy',
+        id: 'createdBy',
+        cell: withPreviewCell,
+      },
+      {
+        header: 'createdDate',
+        accessorKey: 'createdDate',
+        id: 'createdDate',
+        accessorFn(cellInfo) {
+          return datePickerFormat(cellInfo.createdDate)
+        },
+
+        meta: { format: 'date' },
+        cell(cellInfo) {
+          return (
+            <Flex position="relative">
+              <Box mr={2}>{dateFormat(cellInfo.getValue() as string)}</Box>
+              <Spacer w="90px" />
+              <Box
+                left="81%"
+                top="-85%"
+                w="39px"
+                h="46px"
+                position="absolute"
+                onClick={() => {
+                  downloadFileOnly(cellInfo.row.original)
+                }}
+                _hover={{ cursor: 'pointer', backgroundColor: '#CFEDFD' }}
+              >
+                <Icon
+                  p="1px"
+                  as={BiDownArrowCircle}
+                  color="#345EA6"
+                  fontSize={24}
+                  position="relative"
+                  top="10px"
+                  // bottom="0"
+                  left="8px"
+                  // right="0"
+                  // m="auto"
+                />
+              </Box>
+            </Flex>
+          )
+        },
+      },
+
+      {
+        header: '',
+        id: 'deleteBtn',
+        accessorKey: '',
+
+        cell(cellInfo) {
+          return (
+            <>
+              {cellInfo?.row?.original?.label !== 'Project SOW' && (
+                <Flex
+                  ml="-9px"
+                  onClick={() => {
+                    onResetConfirmationModalOpen()
+                    getDocId(cellInfo?.row?.original)
+                  }}
+                >
+                  {/* <Box mr={2}>{dateFormat(cellInfo.getValue() as string)}</Box> */}
+                  <DeleteIcon
+                    style={{
+                      marginRight: '8px',
+                      color: '#345EA6',
+                      fontSize: 18,
+                    }}
+                  />
+                </Flex>
+              )}
+            </>
+          )
+        },
+        accessorFn: () => {
+          return (
+            <div style={{ marginTop: '1px' }}>
+              {/* {cellInfo.parentWorkOrderId ? cellInfo.parentWorkOrderId?.toString() : '- - -'} */}
+            </div>
+          )
+        },
+        size: 20,
+      },
+    ]
+  }, [onResetConfirmationModalOpen])
+  return columns
+}
